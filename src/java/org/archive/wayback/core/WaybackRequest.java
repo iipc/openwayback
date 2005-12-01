1ffbed37c487bbe7251c53d2fbb3c17cc0eb4418
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2005-12-01 02:20:58 +0000

    FEATURE: added getQueryArguments() which will return the CGI arguments to repeat the same query but for a different page of results.
    Also tweaked whitespace and comments
    
    
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@566 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/core/WaybackRequest.java
+++ src/java/org/archive/wayback/core/WaybackRequest.java
@@ -23,7 +23,12 @@
 
 package org.archive.wayback.core;
 
+import java.util.Enumeration;
 import java.util.Properties;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.archive.wayback.query.OpenSearchQueryParser;
 
 /**
  * Abstraction of all the data associated with a users request to the Wayback
@@ -34,12 +39,13 @@ import java.util.Properties;
  */
 public class WaybackRequest {
 	
-	private int resultsPerPage = 1000;
+	private int resultsPerPage = 10;
 	private int pageNum = 1;
 	private Properties filters = new Properties();
 	
 	/**
-	 * Constructor
+	 * Constructor, possibly/probably this should BE a Properties, instead of
+	 * HAVEing a Properties...
 	 */
 	public WaybackRequest() {
 		super();
@@ -73,15 +79,59 @@ public class WaybackRequest {
 		this.resultsPerPage = resultsPerPage;
 	}
 
+	/**
+	 * @param key
+	 * @return boolean, true if the request contains key 'key'
+	 */
 	public boolean containsKey(String key) {
 		return filters.containsKey(key);
 	}
 
+	/**
+	 * @param key
+	 * @return String value for key 'key', or null if no value exists
+	 */
 	public String get(String key) {
 		return (String) filters.get(key);
 	}
 
+	/**
+	 * @param key
+	 * @param value
+	 */
 	public void put(String key, String value) {
 		filters.put(key, value);
+	}
+	
+	/**
+	 * @param pageNum
+	 * @return String hex-encoded GET CGI arguments which will duplicate the
+	 * same request, but for page 'pageNum' of the results  
+	 */
+	public String getQueryArguments (int pageNum) {
+		int numPerPage = resultsPerPage;
+
+		StringBuffer queryString = new StringBuffer("");
+		for (Enumeration e = filters.keys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			String val = (String) filters.get(key);
+			if(queryString.length() > 0) {
+				queryString.append(" ");
+			}
+			queryString.append(key+":"+val);
+		}
+		String escapedQuery = queryString.toString();
+
+		try {
+			
+			escapedQuery = URLEncoder.encode(escapedQuery,"UTF-8");
+			
+		} catch (UnsupportedEncodingException e) {
+			// oops.. what to do?
+			e.printStackTrace();
+		}
+		return OpenSearchQueryParser.SEARCH_QUERY + "=" + escapedQuery + 
+			"&" + OpenSearchQueryParser.SEARCH_RESULTS + "=" + numPerPage +
+			"&" + OpenSearchQueryParser.START_PAGE + "=" + pageNum;
 	}
 }
