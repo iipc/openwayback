03355166018dedc22dca78efaf89eb778beec5d5
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2005-12-01 02:13:02 +0000

    FEATURES: added exposing of SearchResults summary info, include totals matches, matches shown, and primarily added the context of what page of results is being viewed, which includes generation of URLs that will drive to additional pages of results. Also tweaked some comments
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@561 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/query/UIQueryResults.java
+++ src/java/org/archive/wayback/query/UIQueryResults.java
@@ -27,6 +27,8 @@ package org.archive.wayback.query;
 import java.text.ParseException;
 import java.util.Iterator;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.archive.wayback.WaybackConstants;
 import org.archive.wayback.ReplayResultURIConverter;
 import org.archive.wayback.core.SearchResult;
@@ -35,12 +37,19 @@ import org.archive.wayback.core.Timestamp;
 import org.archive.wayback.core.WaybackRequest;
 
 /**
- *
+ * Abstraction class to provide a single, (hopefully) simpler interface to
+ * query results for UI JSPs. Handles slight massaging of SearchResults (to
+ * fix zero-based/one-based numbers) URI fixup and creation for result pages,
+ * and later may provide more functionality for dividing results into columns,
+ * again to simplify UI JSPs.  
  *
  * @author brad
  * @version $Date$, $Revision$
  */
 public class UIQueryResults {
+	
+	private WaybackRequest wbRequest;
+	
 	private String searchUrl;
 
 	private Timestamp startTimestamp;
@@ -51,20 +60,30 @@ public class UIQueryResults {
 
 	private Timestamp lastResultTimestamp;
 
-	private int resultCount;
+	private int resultsReturned;
+	private int resultsMatching;
+	private int resultsPerPage;
+	private int firstResult;
+	private int lastResult;
+	
+	private int numPages;
+	private int curPage;
 
 	private SearchResults results;
-
+	private HttpServletRequest httpRequest;
 	private ReplayResultURIConverter uriConverter;
 
 	/**
-	 * @param wmRequest
+	 * Constructor -- chew search result summaries into format easier for JSPs
+	 * to digest.
+	 *  
+	 * @param httpRequest 
+	 * @param wbRequest 
 	 * @param results
-	 * @param request
-	 * @param replayUI
+	 * @param uriConverter 
 	 * @throws ParseException 
 	 */
-	public UIQueryResults(WaybackRequest wbRequest, SearchResults results,
+	public UIQueryResults(HttpServletRequest httpRequest, WaybackRequest wbRequest, SearchResults results,
 			ReplayResultURIConverter uriConverter) throws ParseException {
 
 		this.searchUrl = wbRequest.get(WaybackConstants.RESULT_URL);
@@ -78,9 +97,25 @@ public class UIQueryResults {
 		this.lastResultTimestamp = Timestamp.parseBefore(results
 				.getLastResultDate());
 
-		this.resultCount = results.getResultCount();
+		this.resultsReturned = Integer.parseInt(results.getFilter(
+				WaybackConstants.RESULTS_NUM_RETURNED));
+		this.resultsMatching = Integer.parseInt(results.getFilter(
+				WaybackConstants.RESULTS_NUM_RESULTS)) - 1;
+		this.resultsPerPage = Integer.parseInt(results.getFilter(
+				WaybackConstants.RESULTS_REQUESTED));
+		this.firstResult = Integer.parseInt(results.getFilter(
+				WaybackConstants.RESULTS_FIRST_RETURNED));
+		this.lastResult = (firstResult + resultsReturned) - 1;
+		
+		// calculate total pages:
+		numPages = Math.round((int) Math.ceil(resultsMatching/resultsPerPage)) 
+			+ 1;
+		curPage = Math.round((int) Math.floor(firstResult/resultsPerPage)) + 1;
+		
 		this.results = results;
 		this.uriConverter = uriConverter;
+		this.wbRequest = wbRequest;
+		this.httpRequest = httpRequest;
 	}
 
 	/**
@@ -102,13 +137,6 @@ public class UIQueryResults {
 	 */
 	public Timestamp getLastResultTimestamp() {
 		return lastResultTimestamp;
-	}
-
-	/**
-	 * @return number of SearchResult objects in response
-	 */
-	public int getResultCount() {
-		return resultCount;
 	}
 
 	/**
@@ -140,12 +168,77 @@ public class UIQueryResults {
 		return uriConverter.makeReplayURI(result);
 	}
 
+	/**
+	 * @return String pretty representation of end date range filter
+	 */
 	public String prettySearchEndDate() {
 		return endTimestamp.prettyDate();
 	}
 
+	/**
+	 * @return String pretty representation of start date range filter
+	 */
 	public String prettySearchStartDate() {
 		return startTimestamp.prettyDate();
 	}
 
+	/**
+	 * @return Returns the firstResult.
+	 */
+	public int getFirstResult() {
+		return firstResult;
+	}
+
+	/**
+	 * @return Returns the resultsMatching.
+	 */
+	public int getResultsMatching() {
+		return resultsMatching;
+	}
+
+	/**
+	 * @return Returns the resultsPerPage.
+	 */
+	public int getResultsPerPage() {
+		return resultsPerPage;
+	}
+
+	/**
+	 * @return Returns the resultsReturned.
+	 */
+	public int getResultsReturned() {
+		return resultsReturned;
+	}
+
+	/**
+	 * @return Returns the curPage.
+	 */
+	public int getCurPage() {
+		return curPage;
+	}
+
+	/**
+	 * @return Returns the numPages.
+	 */
+	public int getNumPages() {
+		return numPages;
+	}
+
+	/**
+	 * @param pageNum
+	 * @return String URL which will drive browser to search results for a
+	 *         different page of results for the same query
+	 */
+	public String urlForPage(int pageNum) {
+		//http://localhost:8080/wayback/query?q=url:peagreenboat.com%20type:urlprefixquery&count=20&start_page=4
+		return httpRequest.getContextPath() + "/query?" +
+			wbRequest.getQueryArguments(pageNum);
+	}
+
+	/**
+	 * @return Returns the lastResult.
+	 */
+	public int getLastResult() {
+		return lastResult;
+	}
 }
