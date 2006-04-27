291d0daffcf5c0f09689cfab55220f513480d906
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-04-27 01:06:32 +0000

    name change: ReplayResultURIConverter => ResultURIConverter
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@997 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ src/java/org/archive/wayback/ResultURIConverter.java
@@ -0,0 +1,65 @@
+/* ReplayURI
+ *
+ * $Id$
+ *
+ * Created on 5:20:43 PM Nov 1, 2005.
+ *
+ * Copyright (C) 2005 Internet Archive.
+ *
+ * This file is part of wayback.
+ *
+ * wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback;
+
+import org.archive.wayback.core.SearchResult;
+
+/**
+ *
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public interface ResultURIConverter extends PropertyConfigurable {
+	/** Convert a SearchResult into a replayable URL
+	 * 
+	 * @param result
+	 * @return user-viewable String URL that will replay the ResourceResult
+	 */
+	public String makeReplayURI(final SearchResult result);
+	/** create a URL that will drive the client back to the URL argument in 
+	 * replay mode, possibly resolving the URL against this SearchResult
+	 * @param result
+	 * @param url
+	 * @return String URL to send client to replay this url
+	 */
+	public String makeRedirectReplayURI(final SearchResult result, String url);
+
+	/** create a URL that will drive the client back to the URL argument in 
+	 * replay mode, resolving the URL against baseUrl
+	 * @param result
+	 * @param url
+	 * @param baseUrl 
+	 * @return String URL to send client to replay this url
+	 */
+	public String makeRedirectReplayURI(final SearchResult result, String url, 
+			String baseUrl);
+	/**
+	 * @param result 
+	 * @return the URL prefix for the replay service, which when another URL
+	 * is appended, will drive back to the replay service
+	 */
+	public String getReplayUriPrefix(final SearchResult result);
+}
