2eedde773854ddddf331dec519d6a1b1a2a2097c
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-04-27 00:23:30 +0000

    FEATURE:
    moved many methods up into new BaseReplayRenderer class
    uses new URICOvnerter class name
    allocate byte buffer only once
    
    
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@977 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/proxy/RawReplayRenderer.java
+++ src/java/org/archive/wayback/proxy/RawReplayRenderer.java
@@ -29,22 +29,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.archive.wayback.ReplayRenderer;
-import org.archive.wayback.ReplayResultURIConverter;
-import org.archive.wayback.WaybackConstants;
+import org.archive.wayback.ResultURIConverter;
 import org.archive.wayback.core.Resource;
 import org.archive.wayback.core.SearchResult;
 import org.archive.wayback.core.WaybackRequest;
-import org.archive.wayback.exception.ConfigurationException;
-import org.archive.wayback.exception.WaybackException;
+import org.archive.wayback.replay.BaseReplayRenderer;
 
 /**
  * 
@@ -52,98 +45,23 @@ import org.archive.wayback.exception.WaybackException;
  * @author brad
  * @version $Date$, $Revision$
  */
-public class RawReplayRenderer implements ReplayRenderer {
-	private final static String JSP_PATH = "replayui.jsppath";
+public class RawReplayRenderer extends BaseReplayRenderer {
+
 	private final static String HTTP_LOCATION_HEADER = "Location";
-	
-	protected final Pattern IMAGE_REGEX = Pattern
-	.compile(".*\\.(jpg|jpeg|gif|png|bmp|tiff|tif)$");
 
-
-	private String jspPath;
-
-	private final String ERROR_JSP = "ErrorResult.jsp";
-	private final String ERROR_JAVASCRIPT = "ErrorJavascript.jsp";
-	private final String ERROR_CSS = "ErrorCSS.jsp";
-	private final String ERROR_IMAGE = "error_image.gif";
-
-	public void init(Properties p) throws ConfigurationException {
-		this.jspPath = (String) p.get(JSP_PATH);
-		if (this.jspPath == null || this.jspPath.length() <= 0) {
-			throw new IllegalArgumentException("Failed to find " + JSP_PATH);
-		}
-	}
-	
-	private boolean requestIsEmbedded(HttpServletRequest httpRequest,
-			WaybackRequest wbRequest) {
-		
-		String referer = wbRequest.get(WaybackConstants.REQUEST_REFERER_URL);
-		return (referer != null && referer.length() > 0);
-	}
-
-	private boolean requestIsImage (HttpServletRequest httpRequest,
-	WaybackRequest wbRequest) {
-		String requestUrl = wbRequest.get(WaybackConstants.REQUEST_URL);
-		Matcher matcher = IMAGE_REGEX.matcher(requestUrl);
-		return (matcher != null && matcher.matches());
-	}
-
-	private boolean requestIsJavascript (HttpServletRequest httpRequest,
-			WaybackRequest wbRequest) {
-			
-		String requestUrl = wbRequest.get(WaybackConstants.REQUEST_URL);
-		return requestUrl.endsWith(".js");
-	}
-
-	private boolean requestIsCSS (HttpServletRequest httpRequest,
-			WaybackRequest wbRequest) {
-			
-		String requestUrl = wbRequest.get(WaybackConstants.REQUEST_URL);
-		return requestUrl.endsWith(".css");
-	}
-
-	// TODO special handling for Javascript and Images: send empty image
-	// or empty text file to avoid client errors
-	public void renderException(HttpServletRequest httpRequest,
-			HttpServletResponse httpResponse, WaybackRequest wbRequest,
-			WaybackException exception) throws ServletException, IOException {
-
-		String finalJspPath = jspPath + "/" + ERROR_JSP;
-
-		// is this object embedded?
-		if(requestIsEmbedded(httpRequest,wbRequest)) {
-			if(requestIsJavascript(httpRequest,wbRequest)) {
-				
-				finalJspPath = jspPath + "/" + ERROR_JAVASCRIPT;
-
-			} else if(requestIsCSS(httpRequest,wbRequest)) {
-
-				finalJspPath = jspPath + "/" + ERROR_CSS;
-				
-			} else if(requestIsImage(httpRequest,wbRequest)) {
-
-				finalJspPath = jspPath + "/" + ERROR_IMAGE;
-				
-			}
-		}
-
-		httpRequest.setAttribute("exception", exception);
-
-		RequestDispatcher dispatcher = httpRequest
-				.getRequestDispatcher(finalJspPath);
-
-		dispatcher.forward(httpRequest, httpResponse);
-	}
+	private final static int BYTE_BUFFER_SIZE = 4 * 1024;
+	// reusable buffer for copying data to clients:
+	private byte[] buffer = new byte[BYTE_BUFFER_SIZE];
 
 	public void renderResource(HttpServletRequest httpRequest,
 			HttpServletResponse httpResponse, WaybackRequest wbRequest,
 			SearchResult result, Resource resource,
-			ReplayResultURIConverter uriConverter) throws ServletException,
+			ResultURIConverter uriConverter) throws ServletException,
 			IOException {
-		
+
 		resource.parseHeaders();
-		copyRecordHttpHeader(httpResponse, resource, uriConverter, result, 
-			false);
+		copyRecordHttpHeader(httpResponse, resource, uriConverter, result,
+				false);
 		copy(resource, httpResponse.getOutputStream());
 	}
 
@@ -151,23 +69,25 @@ public class RawReplayRenderer implements ReplayRenderer {
 	 * callback function for each HTTP header. If null is returned, header is
 	 * omitted from final response to client, otherwise, the possibly modified
 	 * http header value is returned to the client.
-	 * @param key 
-	 * @param value 
-	 * @param uriConverter 
-	 * @param result 
+	 * 
+	 * @param key
+	 * @param value
+	 * @param uriConverter
+	 * @param result
 	 * @return String
 	 */
 	protected String filterHeader(final String key, final String value,
-			final ReplayResultURIConverter uriConverter, SearchResult result) {
+			final ResultURIConverter uriConverter, SearchResult result) {
 		String finalHeaderValue = value;
-		if(0 == key.indexOf(HTTP_LOCATION_HEADER)) {
-			finalHeaderValue = uriConverter.makeRedirectReplayURI(result,value);
+		if (0 == key.indexOf(HTTP_LOCATION_HEADER)) {
+			finalHeaderValue = uriConverter
+					.makeRedirectReplayURI(result, value);
 		}
 		return finalHeaderValue;
 	}
-	
+
 	protected void copyRecordHttpHeader(HttpServletResponse response,
-			Resource resource, ReplayResultURIConverter uriConverter,
+			Resource resource, ResultURIConverter uriConverter,
 			SearchResult result, boolean noLength) throws IOException {
 		Properties headers = resource.getHttpHeaders();
 		int code = resource.getStatusCode();
@@ -187,9 +107,9 @@ public class RawReplayRenderer implements ReplayRenderer {
 				String key = (String) e.nextElement();
 				String value = (String) headers.get(key);
 				String finalValue = value;
-				if(value != null) {
-					finalValue = filterHeader(key,value,uriConverter,result);
-					if(finalValue == null) {
+				if (value != null) {
+					finalValue = filterHeader(key, value, uriConverter, result);
+					if (finalValue == null) {
 						continue;
 					}
 				}
@@ -199,9 +119,7 @@ public class RawReplayRenderer implements ReplayRenderer {
 	}
 
 	protected void copy(InputStream is, OutputStream os) throws IOException {
-		// TODO: Don't allocate everytime.
-		byte[] buffer = new byte[4 * 1024];
-		for (int r = -1; (r = is.read(buffer, 0, buffer.length)) != -1;) {
+		for (int r = -1; (r = is.read(buffer, 0, BYTE_BUFFER_SIZE)) != -1;) {
 			os.write(buffer, 0, r);
 		}
 	}
