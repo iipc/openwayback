cf39ca121de74755caf8ebdf7c3580c868eee4ea
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-10-05 20:09:48 +0000

    WHITESPACE
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1254 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/ResultURIConverter.java
+++ src/java/org/archive/wayback/ResultURIConverter.java
@@ -34,35 +34,42 @@ import org.archive.wayback.core.WaybackRequest;
 import org.archive.wayback.exception.ConfigurationException;
 
 /**
- *
- *
+ * 
+ * 
  * @author brad
  * @version $Date$, $Revision$
  */
 public abstract class ResultURIConverter implements PropertyConfigurable {
 	protected WaybackRequest wbRequest;
+
 	private Properties config = null;
+
 	public void init(Properties p) throws ConfigurationException {
 		config = p;
 	}
+
 	/**
-	 * @param wbRequest The wbRequest to set.
+	 * @param wbRequest
+	 *            The wbRequest to set.
 	 */
 	public void setWbRequest(WaybackRequest wbRequest) {
 		this.wbRequest = wbRequest;
 	}
+
 	protected String getConfigOrContextRelative(String configName, String rel) {
 		String configValue = config.getProperty(configName);
-		if(configValue != null) {
+		if (configValue != null) {
 			return configValue;
 		}
 		return wbRequest.getDefaultWaybackPrefix() + rel;
 	}
+
 	private boolean isAbsolute(String url) {
 		return url.startsWith(WaybackConstants.HTTP_URL_PREFIX);
 	}
+
 	protected String resolveUrl(String url, String baseUrl) {
-		if(!isAbsolute(baseUrl)) {
+		if (!isAbsolute(baseUrl)) {
 			baseUrl = WaybackConstants.HTTP_URL_PREFIX + baseUrl;
 		}
 		UURI absBaseURI;
@@ -76,18 +83,21 @@ public abstract class ResultURIConverter implements PropertyConfigurable {
 		}
 		return resolvedURI.getEscapedURI();
 	}
+
 	protected String searchResultToDateStr(SearchResult result) {
-		return result.get(WaybackConstants.RESULT_CAPTURE_DATE); 		
+		return result.get(WaybackConstants.RESULT_CAPTURE_DATE);
 	}
+
 	protected String searchResultToUrl(SearchResult result) {
 		return result.get(WaybackConstants.RESULT_URL);
 	}
+
 	protected String searchResultToAbsoluteUrl(SearchResult result) {
 		String abs = searchResultToUrl(result);
-		if(abs.startsWith(WaybackConstants.HTTP_URL_PREFIX)) return abs;
+		if (abs.startsWith(WaybackConstants.HTTP_URL_PREFIX))
+			return abs;
 		return WaybackConstants.HTTP_URL_PREFIX + abs;
 	}
-
 
 	/**
 	 * return an absolute URL that will replay URL url at time datespec.
@@ -96,9 +106,10 @@ public abstract class ResultURIConverter implements PropertyConfigurable {
 	 * @param url
 	 * @return absolute replay URL
 	 */
-	public abstract String makeReplayURI(final String datespec,final String url);
-	
-	/** Convert a SearchResult into a replayable URL
+	public abstract String makeReplayURI(final String datespec, final String url);
+
+	/**
+	 * Convert a SearchResult into a replayable URL
 	 * 
 	 * @param result
 	 * @return user-viewable String URL that will replay the ResourceResult
@@ -107,35 +118,41 @@ public abstract class ResultURIConverter implements PropertyConfigurable {
 		return makeReplayURI(searchResultToDateStr(result),
 				searchResultToAbsoluteUrl(result));
 	}
-	/** create a URL that will drive the client back to the URL argument in 
+
+	/**
+	 * create a URL that will drive the client back to the URL argument in
 	 * replay mode, possibly resolving the URL against this SearchResult
+	 * 
 	 * @param result
 	 * @param url
 	 * @return String URL to send client to replay this url
 	 */
 	public String makeRedirectReplayURI(final SearchResult result, String url) {
-		String finalUrl = resolveUrl(searchResultToUrl(result),url);
-		return makeReplayURI(searchResultToDateStr(result),finalUrl);
+		String finalUrl = resolveUrl(searchResultToUrl(result), url);
+		return makeReplayURI(searchResultToDateStr(result), finalUrl);
 	}
 
-	/** create a URL that will drive the client back to the URL argument in 
+	/**
+	 * create a URL that will drive the client back to the URL argument in
 	 * replay mode, resolving the URL against baseUrl
+	 * 
 	 * @param result
 	 * @param url
-	 * @param baseUrl 
+	 * @param baseUrl
 	 * @return String URL to send client to replay this url
 	 */
-	public String makeRedirectReplayURI(final SearchResult result, String url, 
+	public String makeRedirectReplayURI(final SearchResult result, String url,
 			String baseUrl) {
-		return makeReplayURI(searchResultToDateStr(result),
-				resolveUrl(url,baseUrl));
+		return makeReplayURI(searchResultToDateStr(result), resolveUrl(url,
+				baseUrl));
 	}
+
 	/**
-	 * @param result 
-	 * @return the URL prefix for the replay service, which when another URL
-	 * is appended, will drive back to the replay service
+	 * @param result
+	 * @return the URL prefix for the replay service, which when another URL is
+	 *         appended, will drive back to the replay service
 	 */
 	public String getReplayUriPrefix(final SearchResult result) {
-		return makeReplayURI(searchResultToDateStr(result),"");
+		return makeReplayURI(searchResultToDateStr(result), "");
 	}
 }
