642857a3e38903c49028ae5b9ac6b92c6f59dda8
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-10-17 21:59:12 +0000

    INITIAL-REV: interface for iterator that can be(read: needs to be) closed to free underlying resources.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1264 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ src/java/org/archive/wayback/util/CloseableIterator.java
@@ -0,0 +1,38 @@
+/* Cleanable
+ *
+ * $Id$
+ *
+ * Created on 1:44:45 PM Aug 18, 2006.
+ *
+ * Copyright (C) 2006 Internet Archive.
+ *
+ * This file is part of Wayback.
+ *
+ * Wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.util;
+
+import java.io.Closeable;
+import java.util.Iterator;
+
+/**
+ * Iterator with a close method that frees up any resources associated with 
+ * the Iterator.
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public interface CloseableIterator extends Iterator, Closeable {
+}
