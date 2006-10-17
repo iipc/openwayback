a8027172423c28c4495fe48267799ca5e88fb9b8
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-10-17 22:23:38 +0000

    INITIAL-REV: SearchResultSource implementation based on a sort flat file source.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1278 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ src/java/org/archive/wayback/resourceindex/cdx/CDXIndex.java
@@ -0,0 +1,160 @@
+/* LocalCDXResourceIndex
+ *
+ * $Id$
+ *
+ * Created on 2:15:12 PM Aug 17, 2006.
+ *
+ * Copyright (C) 2006 Internet Archive.
+ *
+ * This file is part of Wayback.
+ *
+ * Wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.resourceindex.cdx;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Iterator;
+import org.archive.wayback.WaybackConstants;
+import org.archive.wayback.core.SearchResult;
+import org.archive.wayback.core.Timestamp;
+import org.archive.wayback.exception.ResourceIndexNotAvailableException;
+import org.archive.wayback.resourceindex.SearchResultSource;
+import org.archive.wayback.util.AdaptedIterator;
+import org.archive.wayback.util.CloseableIterator;
+import org.archive.wayback.util.CompositeSortedIterator;
+import org.archive.wayback.util.flatfile.FlatFile;
+
+/**
+ * 
+ * 
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public class CDXIndex extends FlatFile implements SearchResultSource {
+
+	/**
+	 * @param parent
+	 * @param child
+	 */
+	public CDXIndex(File parent, String child) {
+		super(parent, child);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param path
+	 */
+	public CDXIndex(String path) {
+		super(path);
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private CloseableIterator adaptIterator(Iterator itr) {
+		return new AdaptedIterator(itr,new CDXLineToSearchResultAdapter());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.wayback.resourceindex.SearchResultSource#getPrefixIterator(java.lang.String)
+	 */
+	public CloseableIterator getPrefixIterator(String prefix)
+		throws ResourceIndexNotAvailableException {
+		try {
+			return adaptIterator(getRecordIterator(prefix));
+		} catch (IOException e) {
+			throw new ResourceIndexNotAvailableException(e.getMessage()); 
+		}
+	}
+	/* (non-Javadoc)
+	 * @see org.archive.wayback.resourceindex.SearchResultSource#getPrefixReverseIterator(java.lang.String)
+	 */
+	public CloseableIterator getPrefixReverseIterator(String prefix)
+		throws ResourceIndexNotAvailableException {
+		try {
+			return adaptIterator(getReverseRecordIterator(prefix));
+		} catch (IOException e) {
+			throw new ResourceIndexNotAvailableException(e.getMessage()); 
+		}
+	}
+
+	/**
+	 * @param prefix
+	 * @return Iterator of SearchResults of records starting with prefix
+	 * @throws IOException
+	 */
+	public Iterator getUrlIterator(final String prefix) throws IOException {
+		return adaptIterator(getRecordIterator(prefix));
+	}
+	
+	/**
+	 * @param prefix
+	 * @param wantTS
+	 * @return Iterator of results in closest order to wantTS
+	 * @throws IOException
+	 */
+	public Iterator getClosestIterator(final String prefix, 
+			final Timestamp wantTS) throws IOException {
+		
+		Iterator forwardItr = adaptIterator(getRecordIterator(prefix));
+		Iterator reverseItr = adaptIterator(getReverseRecordIterator(prefix));
+		TimestampComparator comparator = new TimestampComparator(wantTS);
+		CompositeSortedIterator itr = new CompositeSortedIterator(comparator);
+		itr.addComponent(forwardItr);
+		itr.addComponent(reverseItr);
+		return itr;
+	}
+
+	private class TimestampComparator implements Comparator {
+		private int wantedSSE;
+		/**
+		 * @param wanted
+		 */
+		public TimestampComparator(Timestamp wanted) {
+			wantedSSE = wanted.sse();
+		}
+		private int searchResultToDistance(Object o) {
+			if(!(o instanceof SearchResult)) {
+				throw new IllegalArgumentException("Need SearchResult objects");
+			}
+			SearchResult sr = (SearchResult) o;
+			String dateStr = sr.get(WaybackConstants.RESULT_CAPTURE_DATE);
+			Timestamp ts = new Timestamp(dateStr);
+			return Math.abs(wantedSSE - ts.sse());
+		}
+		/* (non-Javadoc)
+		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+		 */
+		public int compare(Object o1, Object o2) {
+			int d1 = searchResultToDistance(o1);
+			int d2 = searchResultToDistance(o2);
+			if(d1 < d2) {
+				return -1;
+			} else if(d1 > d2) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.wayback.resourceindex.SearchResultSource#cleanup(org.archive.wayback.util.CleanableIterator)
+	 */
+	public void cleanup(CloseableIterator c) throws IOException {
+		c.close();
+	}
+}
---
+++ src/java/org/archive/wayback/resourceindex/cdx/CDXLineToSearchResultAdapter.java
@@ -0,0 +1,89 @@
+/* CDXLineToSearchResultAdaptor
+ *
+ * $Id$
+ *
+ * Created on 2:27:16 PM Aug 17, 2006.
+ *
+ * Copyright (C) 2006 Internet Archive.
+ *
+ * This file is part of Wayback.
+ *
+ * Wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.resourceindex.cdx;
+
+
+import org.apache.commons.httpclient.URIException;
+import org.archive.net.UURI;
+import org.archive.net.UURIFactory;
+import org.archive.wayback.WaybackConstants;
+import org.archive.wayback.core.SearchResult;
+import org.archive.wayback.util.Adapter;
+
+/**
+ * Adapter that converts a CDX record String into a SearchResult
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public class CDXLineToSearchResultAdapter implements Adapter {
+
+	public Object adapt(Object o) {
+		SearchResult result = new SearchResult();
+		if(!(o instanceof String)) {
+			throw new IllegalArgumentException("Argument is not a String");
+		}
+		String line = (String) o;
+		String[] tokens = line.split(" ");
+		if (tokens.length != 9) {
+			return null;
+			//throw new IllegalArgumentException("Need 9 columns("+line+")");
+		}
+		String url = tokens[0];
+		String captureDate = tokens[1];
+		String origHost = tokens[2];
+		String mimeType = tokens[3];
+		String httpResponseCode = tokens[4];
+		String md5Fragment = tokens[5];
+		String redirectUrl = tokens[6];
+		long compressedOffset = Long.parseLong(tokens[7]);
+		String arcFileName = tokens[8];
+
+		String origUrl = url;
+		try {
+			UURI uri = UURIFactory.getInstance(WaybackConstants.HTTP_URL_PREFIX +
+					url);
+			origUrl = origHost + uri.getEscapedPathQuery();
+		} catch (URIException e) {
+			// TODO Stifle? throw an error?
+			e.printStackTrace();
+			return null;
+		}
+		
+		result.put(WaybackConstants.RESULT_URL, origUrl);
+		result.put(WaybackConstants.RESULT_URL_KEY, url);
+		result.put(WaybackConstants.RESULT_CAPTURE_DATE, captureDate);
+		result.put(WaybackConstants.RESULT_ORIG_HOST, origHost);
+		result.put(WaybackConstants.RESULT_MIME_TYPE, mimeType);
+		result.put(WaybackConstants.RESULT_HTTP_CODE, httpResponseCode);
+		result.put(WaybackConstants.RESULT_MD5_DIGEST, md5Fragment);
+		result.put(WaybackConstants.RESULT_REDIRECT_URL, redirectUrl);
+		// HACKHACK:
+		result.put(WaybackConstants.RESULT_OFFSET, String.valueOf(compressedOffset));
+		result.put(WaybackConstants.RESULT_ARC_FILE, arcFileName);
+
+		return result;
+	}
+}
