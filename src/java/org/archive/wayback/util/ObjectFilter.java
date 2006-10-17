336ce294c3aa9878b88c97d96e9d46790c080740
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2006-10-17 22:02:10 +0000

    INITIAL-REV: interface for including, excluding, or aborting scanning thru a series of records. Also includes a composite/chain class that implements the interface, allowing multiple filters to be run in sequence.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1266 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ src/java/org/archive/wayback/util/ObjectFilter.java
@@ -0,0 +1,57 @@
+/* ObjectFilter
+ *
+ * $Id$
+ *
+ * Created on 3:10:51 PM Aug 17, 2006.
+ *
+ * Copyright (C) 2006 Internet Archive.
+ *
+ * This file is part of Wayback.
+ *
+ * Wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.util;
+
+/**
+ * Interface for including, excluding, or aborting filtering
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public interface ObjectFilter {
+	/**
+	 * constant indicating record should be included in the result set
+	 */
+	public static int FILTER_INCLUDE = 0;
+	
+	/**
+	 * constant indicating record should be omitted from the result set
+	 */
+	public static int FILTER_EXCLUDE = 1;
+	
+	/**
+	 * constant indicating record should be ommitted, and no more records should
+	 * be processed.
+	 */
+	public static int FILTER_ABORT = 2;
+	
+	/** inpect record and determine if it should be included in the 
+	 * results or not, or if processing of new records should stop.
+	 * @param o Object which should be checked for inclusion/exclusion or abort
+	 * @return int of FILTER_INCLUDE, FILTER_EXCLUDE, or FILTER_ABORT
+	 */
+	public int filterObject(Object o);
+
+}
---
+++ src/java/org/archive/wayback/util/ObjectFilterChain.java
@@ -0,0 +1,75 @@
+/* ObjectFilterChain
+ *
+ * $Id$
+ *
+ * Created on 3:12:35 PM Aug 17, 2006.
+ *
+ * Copyright (C) 2006 Internet Archive.
+ *
+ * This file is part of Wayback.
+ *
+ * Wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * Wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with Wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.util;
+
+import java.util.ArrayList;
+
+/**
+ * ObjectFilterChain implements AND logic to chain together multiple 
+ * ObjectFilters into a composite. ABORT and EXCLUDE short circuit the chain, 
+ * all filters must INCLUDE for inclusion.
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+
+public class ObjectFilterChain implements ObjectFilter {
+
+	private ArrayList filters = null;
+
+	/**
+	 * Constructor
+	 */
+	public ObjectFilterChain() {
+		this.filters = new ArrayList();
+	}
+
+	/**
+	 * @param filter to be added to the chain. filters are processed in the 
+	 * order they are added to the chain.
+	 */
+	public void addFilter(ObjectFilter filter) {
+		filters.add(filter);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.archive.wayback.cdx.filter.RecordFilter#filterRecord(org.archive.wayback.cdx.CDXRecord)
+	 */
+	public int filterObject(Object o) {
+
+		int size = filters.size();
+		int result = FILTER_ABORT;
+		for (int i = 0; i < size; i++) {
+			ObjectFilter filter = (ObjectFilter) filters.get(i);
+			result = filter.filterObject(o);
+			if (result == FILTER_ABORT) {
+				break;
+			} else if (result == FILTER_EXCLUDE) {
+				break;
+			}
+		}
+		return result;
+	}
+}
