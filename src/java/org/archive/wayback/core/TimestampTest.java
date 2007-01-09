2f3748533d948811151754ec5ea3cf997ea905b1
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-01-09 00:38:16 +0000

    BUGFIX: there is a hard-coded end date. was 2006, now is 2007.. Still needs permanent solution.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1413 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/core/TimestampTest.java
+++ src/java/org/archive/wayback/core/TimestampTest.java
@@ -43,11 +43,11 @@ public class TimestampTest extends TestCase {
 		assertEquals("padStart '1'","19960101000000",Timestamp.padStartDateStr("1"));
 		assertEquals("padEnd '1'","19991231235959",Timestamp.padEndDateStr("1"));
 		assertEquals("padStart '2'","20000101000000",Timestamp.padStartDateStr("2"));
-		assertEquals("padEnd","20061231235959",Timestamp.padEndDateStr("2"));
-		assertEquals("padEnd","20061231235959",Timestamp.padEndDateStr("3"));
+		assertEquals("padEnd","20071231235959",Timestamp.padEndDateStr("2"));
+		assertEquals("padEnd","20071231235959",Timestamp.padEndDateStr("3"));
 		assertEquals("padEnd","20061231235959",Timestamp.padEndDateStr("2006"));
 		assertEquals("padEnd","20061231235959",Timestamp.padEndDateStr("200613"));
-		assertEquals("padEnd","20061231235959",Timestamp.padEndDateStr("2007"));
+		assertEquals("padEnd","20071231235959",Timestamp.padEndDateStr("2007"));
 		
 
 		// day of month stuff:
