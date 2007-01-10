27f7f883fd2b404c1d8575a2ce0fd0501d96eb51
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-01-10 23:43:57 +0000

    FEATURE: added main() to support command line tool bdb-client, which allows dumping of records to/from a local BDB.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1414 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- src/java/org/archive/wayback/resourceindex/bdb/BDBIndex.java
+++ src/java/org/archive/wayback/resourceindex/bdb/BDBIndex.java
@@ -24,12 +24,20 @@
  */
 package org.archive.wayback.resourceindex.bdb;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Iterator;
 
+import org.archive.wayback.WaybackConstants;
 import org.archive.wayback.bdb.BDBRecordSet;
+import org.archive.wayback.core.SearchResult;
+import org.archive.wayback.core.SearchResults;
 import org.archive.wayback.exception.ResourceIndexNotAvailableException;
 import org.archive.wayback.resourceindex.SearchResultSource;
+import org.archive.wayback.resourceindex.indexer.ArcIndexer;
 import org.archive.wayback.util.AdaptedIterator;
 import org.archive.wayback.util.CloseableIterator;
 
@@ -82,5 +90,158 @@ public class BDBIndex extends BDBRecordSet implements SearchResultSource {
 	public void cleanup(CloseableIterator c) throws IOException {
 		c.close();
 	}
+	
+	private static void USAGE() {
+		System.err.println("Usage: DBPATH DBNAME -w");
+		System.err.println("\tRead lines from STDIN, inserting into BDBJE at\n" +
+				" DBPATH named DBNAME, creating DB if needed.");
 
+		System.err.println("Usage: DBPATH DBNAME -r [PREFIX]");
+		System.err.println("\tDump lines from BDBJE at path DBPATH named " +
+				"DBNAME\n to STDOUT. If PREFIX is specified, only output " +
+				"records\n beginning with PREFIX, otherwise output all " +
+				"records");
+		
+		System.exit(3);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		if(args.length < 3) {
+			USAGE();
+		}
+		String path = args[0];
+		String name = args[1];
+		String op = args[2];
+		BDBIndex index = new BDBIndex();
+		int BATCH_SIZE = 1000;
+		ArcIndexer indexer = new ArcIndexer();
+
+		try {
+			index.initializeDB(path,name);
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		if(op.compareTo("-r") == 0) {
+			PrintWriter pw = new PrintWriter(System.out);
+			SearchResults results = new SearchResults();
+			if(args.length == 4) {
+				String prefix = args[3];
+				CloseableIterator itr = null;
+				try {
+					itr = index.getPrefixIterator(prefix);
+				} catch (ResourceIndexNotAvailableException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+				while(itr.hasNext()) {
+					SearchResult result = (SearchResult) itr.next();
+					String urlS = result.get(WaybackConstants.RESULT_URL_KEY);
+					if(!urlS.startsWith(prefix)) {
+						break;
+					}
+					results.addSearchResult(result);
+					if(results.getResultCount() > BATCH_SIZE) {
+						try {
+							indexer.serializeResults(results,pw,false);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(2);
+						}
+						results = new SearchResults();
+					}
+				}
+				if(results.getResultCount() > 0) {
+					try {
+						indexer.serializeResults(results,pw,false);
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(2);
+					}
+				}
+			} else {
+				CloseableIterator itr = null;
+				try {
+					itr = index.getPrefixIterator(" ");
+				} catch (ResourceIndexNotAvailableException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+				while(itr.hasNext()) {
+					SearchResult result = (SearchResult) itr.next();
+					results.addSearchResult(result);
+					if(results.getResultCount() > BATCH_SIZE) {
+						try {
+							indexer.serializeResults(results,pw,false);
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(2);
+						}
+						results = new SearchResults();
+					}
+				}
+				if(results.getResultCount() > 0) {
+					try {
+						indexer.serializeResults(results,pw,false);
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(2);
+					}
+				}
+				pw.flush();
+				pw.close();
+				
+			}
+			
+		} else if(op.compareTo("-w") == 0) {
+			File tmpCDX = null;
+			int total = 0;
+			int numInTmp = 0;
+			try {
+				tmpCDX = File.createTempFile("reader",".cdx");
+				PrintWriter pw = new PrintWriter(tmpCDX);
+				// need to break the results from STDIN into chunks -- each chunk
+				// is written to a file, then added to the index.
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(System.in));
+				
+				while(true) {
+					String line = br.readLine();
+					if(line == null) {
+						break;
+					}
+					pw.println(line);
+					numInTmp++;
+					total++;
+					if(numInTmp > BATCH_SIZE) {
+						pw.flush();
+						pw.close();
+						index.insertRecords(
+								indexer.getCDXFileBDBRecordIterator(tmpCDX));
+						System.err.println("Wrote " + numInTmp + " to index..");
+						pw = new PrintWriter(tmpCDX);
+						numInTmp = 0;
+					}
+				}
+				if(numInTmp > 0) {
+					pw.flush();
+					pw.close();
+					index.insertRecords(
+							indexer.getCDXFileBDBRecordIterator(tmpCDX));					
+					System.err.println("Wrote last " + numInTmp + " to index.");
+				}
+				tmpCDX.delete();
+				System.out.println("Total of " + total + " docs inserted.");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		} else {
+			USAGE();
+		}
+	}
 }
