ea78c5608cd398f803fcb3befa0934ee845d1863
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-03-30 23:11:30 +0000

    INITAL REV: (unused presently) some test cases that specify files on an archive.org internal server, for the moment, these tests are no-ops, until I find some more reasonable URLs to use for download sources..
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1663 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ src/java/org/archive/wayback/util/CachedFileTest.java
@@ -0,0 +1,62 @@
+/* CachedFileTest
+ *
+ * $Id$
+ *
+ * Created on 3:44:24 PM Jan 25, 2007.
+ *
+ * Copyright (C) 2007 Internet Archive.
+ *
+ * This file is part of wayback-svn.
+ *
+ * wayback-svn is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback-svn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback-svn; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.util;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public class CachedFileTest extends TestCase {
+	/**
+	 * @throws Exception
+	 */
+	public void testCachedFile() throws Exception {
+//		File file = File.createTempFile("tmp","del");
+//		URL url = new URL("http://homeserver.us.archive.org/~brad/tmp.del");
+//		long checkMS = 10;
+//		CachedFile cf = new CachedFile(file,url,checkMS);
+//		Iterator itr = cf.getSequentialIterator();
+//		assertTrue(itr.hasNext());
+//		assertEquals("a one two three",(String)itr.next());
+//		assertTrue(itr.hasNext());
+//		assertEquals("b four five size",(String)itr.next());
+//		assertTrue(itr.hasNext());
+//		assertEquals("c seven eight",(String)itr.next());
+//		assertTrue(itr.hasNext());
+//		assertEquals("d nine ten",(String)itr.next());
+//		assertTrue(itr.hasNext());
+//		assertEquals("e eleven twelve thirteen fourteen fifteen",(String)itr.next());
+//		assertFalse(itr.hasNext());
+//		file.delete();
+	}
+}
