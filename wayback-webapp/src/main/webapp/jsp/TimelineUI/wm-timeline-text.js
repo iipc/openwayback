748aa56fc8ea56ed61b26d7d38501671d9f31f4c
stack-sf <stack-sf@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-04-02 23:05:21 +0000

    D    webapp     Deleted.  Replacing with moved original (this was a copy).
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1675 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-webapp/src/main/webapp/jsp/TimelineUI/wm-timeline-text.js
+++
@@ -1,5 +0,0 @@
-/*  These variables are used in the "common" wm-timeline.js    */			
-var wmNotice = "Wayback - External links, forms, and search boxes may not " +
-               "function within this collection. Url: " + sResourceUrl + 
-               " time: " + sCaptureTS;
-var wmHideNotice = "hide";
