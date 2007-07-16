64b48607000ef7948fa543ad1bb199a6a2bb6629
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-07-16 22:09:53 +0000

    INITIAL REV: New core interface, enabling new WaybackContext service entry point.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1764 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/RequestParser.java
@@ -0,0 +1,51 @@
+/* RequestParser
+ *
+ * $Id$
+ *
+ * Created on 6:42:13 PM Apr 23, 2007.
+ *
+ * Copyright (C) 2007 Internet Archive.
+ *
+ * This file is part of wayback-core.
+ *
+ * wayback-core is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback-core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback-core; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.archive.wayback.core.WaybackRequest;
+import org.archive.wayback.exception.BadQueryException;
+import org.archive.wayback.webapp.WaybackContext;
+
+/**
+ *
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public interface RequestParser extends PropertyConfigurable {
+	/**
+	 * attempt to transform an incoming HttpServletRequest into a 
+	 * WaybackRequest object. returns null if there is missing information.
+	 * 
+	 * @param httpRequest
+	 * @param wbContext 
+	 * @return populated WaybackRequest object if successful, null otherwise.
+	 * @throws BadQueryException 
+	 */
+	public abstract WaybackRequest parse(HttpServletRequest httpRequest, 
+			WaybackContext wbContext) throws BadQueryException;
+}
