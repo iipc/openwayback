dd9b736ddfa534332eaa7797ad53aad35edb0e0c
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-07-16 23:49:43 +0000

    REFACTOR: ResultURIConverter is once again an interface, not an incredibly complicated abstract class. Most of the previous complexity made a little bit of sense given the hoops we needed to jump thru for timeline mode, but since this is being merged with Archival URL, it's gotten the axe.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1801 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/ResultURIConverter.java
+++ wayback-core/src/main/java/org/archive/wayback/ResultURIConverter.java
@@ -24,81 +24,13 @@
  */
 package org.archive.wayback;
 
-import java.util.Properties;
-
-import org.apache.commons.httpclient.URIException;
-import org.archive.net.UURI;
-import org.archive.net.UURIFactory;
-import org.archive.wayback.core.SearchResult;
-import org.archive.wayback.core.WaybackRequest;
-import org.archive.wayback.exception.ConfigurationException;
-
 /**
  * 
  * 
  * @author brad
  * @version $Date$, $Revision$
  */
-public abstract class ResultURIConverter implements PropertyConfigurable {
-	protected WaybackRequest wbRequest;
-
-	private Properties config = null;
-
-	public void init(Properties p) throws ConfigurationException {
-		config = p;
-	}
-
-	/**
-	 * @param wbRequest
-	 *            The wbRequest to set.
-	 */
-	public void setWbRequest(WaybackRequest wbRequest) {
-		this.wbRequest = wbRequest;
-	}
-
-	protected String getConfigOrContextRelative(String configName, String rel) {
-		String configValue = config.getProperty(configName);
-		if (configValue != null) {
-			return configValue;
-		}
-		return wbRequest.getDefaultWaybackPrefix() + rel;
-	}
-
-	private boolean isAbsolute(String url) {
-		return url.startsWith(WaybackConstants.HTTP_URL_PREFIX);
-	}
-
-	protected String resolveUrl(String url, String baseUrl) {
-		if (!isAbsolute(baseUrl)) {
-			baseUrl = WaybackConstants.HTTP_URL_PREFIX + baseUrl;
-		}
-		UURI absBaseURI;
-		UURI resolvedURI = null;
-		try {
-			absBaseURI = UURIFactory.getInstance(baseUrl);
-			resolvedURI = UURIFactory.getInstance(absBaseURI, url);
-		} catch (URIException e) {
-			e.printStackTrace();
-			return url;
-		}
-		return resolvedURI.getEscapedURI();
-	}
-
-	protected String searchResultToDateStr(SearchResult result) {
-		return result.get(WaybackConstants.RESULT_CAPTURE_DATE);
-	}
-
-	protected String searchResultToUrl(SearchResult result) {
-		return result.get(WaybackConstants.RESULT_URL);
-	}
-
-	protected String searchResultToAbsoluteUrl(SearchResult result) {
-		String abs = searchResultToUrl(result);
-		if (abs.startsWith(WaybackConstants.HTTP_URL_PREFIX))
-			return abs;
-		return WaybackConstants.HTTP_URL_PREFIX + abs;
-	}
-
+public interface ResultURIConverter extends PropertyConfigurable {
 	/**
 	 * return an absolute URL that will replay URL url at time datespec.
 	 * 
@@ -106,64 +38,5 @@ public abstract class ResultURIConverter implements PropertyConfigurable {
 	 * @param url
 	 * @return absolute replay URL
 	 */
-	public abstract String makeReplayURI(final String datespec, final String url);
-
-	/**
-	 * Convert a SearchResult into a replayable URL
-	 * 
-	 * @param result
-	 * @return user-viewable String URL that will replay the ResourceResult
-	 */
-	public String makeReplayURI(final SearchResult result) {
-		return makeReplayURI(searchResultToDateStr(result),
-				searchResultToAbsoluteUrl(result));
-	}
-
-	/**
-	 * Convert a SearchResult into an URL that will result in a Query for all
-	 * versions of a the URL represented in the SearchResult argument
-	 * 
-	 * @param result
-	 * @return user-viewable String URL that will replay the ResourceResult
-	 */
-	public String makeQueryURI(final SearchResult result) {
-		return makeReplayURI("*",searchResultToAbsoluteUrl(result));
-	}
-
-	/**
-	 * create a URL that will drive the client back to the URL argument in
-	 * replay mode, possibly resolving the URL against this SearchResult
-	 * 
-	 * @param result
-	 * @param url
-	 * @return String URL to send client to replay this url
-	 */
-	public String makeRedirectReplayURI(final SearchResult result, String url) {
-		String finalUrl = resolveUrl(url,searchResultToUrl(result));
-		return makeReplayURI(searchResultToDateStr(result), finalUrl);
-	}
-
-	/**
-	 * create a URL that will drive the client back to the URL argument in
-	 * replay mode, resolving the URL against baseUrl
-	 * 
-	 * @param result
-	 * @param url
-	 * @param baseUrl
-	 * @return String URL to send client to replay this url
-	 */
-	public String makeRedirectReplayURI(final SearchResult result, String url,
-			String baseUrl) {
-		return makeReplayURI(searchResultToDateStr(result), resolveUrl(url,
-				baseUrl));
-	}
-
-	/**
-	 * @param result
-	 * @return the URL prefix for the replay service, which when another URL is
-	 *         appended, will drive back to the replay service
-	 */
-	public String getReplayUriPrefix(final SearchResult result) {
-		return makeReplayURI(searchResultToDateStr(result), "");
-	}
+	public String makeReplayURI(final String datespec, final String url);
 }
