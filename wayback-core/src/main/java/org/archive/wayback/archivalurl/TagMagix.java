13e1b35ddd4d2203b4169d40570e8163b11ee915
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-07-16 22:18:43 +0000

    FEATURE: Now includes the capability to alter specific attributes on tags, including the capability to only change attributes on tags where the tag includes another attribute with a specific value.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@1766 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/archivalurl/TagMagix.java
+++ wayback-core/src/main/java/org/archive/wayback/archivalurl/TagMagix.java
@@ -27,31 +27,42 @@ package org.archive.wayback.archivalurl;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.archive.wayback.ResultURIConverter;
-import org.archive.wayback.core.SearchResult;
+import org.archive.wayback.util.UrlCanonicalizer;
 
 /**
- * Library for updating arbitrary attributes in arbitrary tags to rewrite
- * HTML documents so URI references point back into the Wayback Machine.
- * Attempts to make minimal changes so nothing gets broken during this process.
- *
+ * Library for updating arbitrary attributes in arbitrary tags to rewrite HTML
+ * documents so URI references point back into the Wayback Machine. Attempts to
+ * make minimal changes so nothing gets broken during this process.
+ * 
  * @author brad
- * @version $Date$, $Revision$
+ * @version $Date$, $Revision:
+ *          1668 $
  */
 public class TagMagix {
 
-	private static HashMap<String,Pattern> pcPatterns = 
-		new HashMap<String,Pattern>();
-	
-	 
-	private static String QUOTED_ATTR_VALUE= "(?:\"[^\">]*\")";
-	private static String ESC_QUOTED_ATTR_VALUE= "(?:\\\\\"[^>\\\\]*\\\\\")";
+	private static HashMap<String, Pattern> pcPatterns = 
+		new HashMap<String, Pattern>();
+
+	private static HashMap<String, Pattern> wholeTagPatterns = 
+		new HashMap<String, Pattern>();
+
+	private static HashMap<String, Pattern> attrPatterns = 
+		new HashMap<String, Pattern>();
+
+	private static String QUOTED_ATTR_VALUE = "(?:\"[^\">]*\")";
+
+	private static String ESC_QUOTED_ATTR_VALUE = "(?:\\\\\"[^>\\\\]*\\\\\")";
+
 	private static String APOSED_ATTR_VALUE = "(?:'[^'>]*')";
+
 	private static String RAW_ATTR_VALUE = "(?:[^ \\t\\n\\x0B\\f\\r>\"']+)";
-	
-	private static String ANY_ATTR_VALUE = QUOTED_ATTR_VALUE+ "|" + APOSED_ATTR_VALUE +
-		"|" + ESC_QUOTED_ATTR_VALUE + "|" + RAW_ATTR_VALUE;
-	
+
+	private static String ANY_ATTR_VALUE = QUOTED_ATTR_VALUE + "|"
+			+ APOSED_ATTR_VALUE + "|" + ESC_QUOTED_ATTR_VALUE + "|"
+			+ RAW_ATTR_VALUE;
+
 	/**
 	 * get (and cache) a regex Pattern for locating an HTML attribute value
 	 * within a particular tag. if found, the pattern will have the attribute
@@ -62,70 +73,204 @@ public class TagMagix {
 	 * @param attrName
 	 * @return Pattern to match the tag-attribute's value
 	 */
-	private synchronized static Pattern getPattern(String tagName, String attrName) {
+	private synchronized static Pattern getPattern(String tagName,
+			String attrName) {
 
 		String key = tagName + "    " + attrName;
 		Pattern pc = pcPatterns.get(key);
-		if(pc == null) {
-			
-			String tagPatString = "<\\s*" + tagName + "\\s+[^>]*\\b" + attrName + 
-				"\\s*=\\s*(" + ANY_ATTR_VALUE + ")(?:\\s|>)?";
-			
-			pc = Pattern.compile(tagPatString,Pattern.CASE_INSENSITIVE);
-			pcPatterns.put(key,pc);
+		if (pc == null) {
+
+			String tagPatString = "<\\s*" + tagName + "\\s+[^>]*\\b" + attrName
+					+ "\\s*=\\s*(" + ANY_ATTR_VALUE + ")(?:\\s|>)?";
+
+			pc = Pattern.compile(tagPatString, Pattern.CASE_INSENSITIVE);
+			pcPatterns.put(key, pc);
 		}
 		return pc;
 	}
 
 	/**
-	 * Alter the HTML document in page, updating URLs in the attrName 
-	 * attributes of all tagName tags such that:
+	 * get (and cache) a regex Pattern for locating an entire HTML start tag.
 	 * 
-	 *   1) absolute URLs are prefixed with:
-	 *         wmPrefix + pageTS
-	 * 	 2) server-relative URLs are prefixed with:
-	 *         wmPrefix + pageTS + (host of page)
-	 *   3) path-relative URLs are prefixed with:
-	 *         wmPrefix + pageTS + (attribute URL resolved against pageUrl)
+	 * @param tagName
+	 * @return Pattern to match the tag
+	 */
+	private synchronized static Pattern getWholeTagPattern(String tagName) {
+
+		Pattern pc = wholeTagPatterns.get(tagName);
+		if (pc == null) {
+
+			String tagPatString = "<\\s*" + tagName + "\\s+[^>]+>";
+
+			pc = Pattern.compile(tagPatString, Pattern.CASE_INSENSITIVE);
+			wholeTagPatterns.put(tagName, pc);
+		}
+		return pc;
+	}
+
+	/**
+	 * get (and cache) a regex Pattern for locating an attribute value within an
+	 * HTML start tag. If this pattern matches, the attribute value will be in
+	 * group(1), and will include surrounding quotes, or apos, if they were
+	 * present in the original HTML.
+	 * 
+	 * @param attrName
+	 * @return Pattern to match the attributes value
+	 */
+	private synchronized static Pattern getAttrPattern(String attrName) {
+
+		Pattern pc = attrPatterns.get(attrName);
+		if (pc == null) {
+
+			String attrPatString = "\\b" + attrName + "\\s*=\\s*("
+					+ ANY_ATTR_VALUE + ")(?:\\s|>)?";
+
+			pc = Pattern.compile(attrPatString, Pattern.CASE_INSENSITIVE);
+			attrPatterns.put(attrName, pc);
+		}
+		return pc;
+	}
+
+	/**
+	 * Alter the HTML document in page, updating URLs in the attrName attributes
+	 * of all tagName tags such that:
+	 * 
+	 * 1) absolute URLs are prefixed with: wmPrefix + pageTS 2) server-relative
+	 * URLs are prefixed with: wmPrefix + pageTS + (host of page) 3)
+	 * path-relative URLs are prefixed with: wmPrefix + pageTS + (attribute URL
+	 * resolved against pageUrl)
 	 * 
 	 * @param page
-	 * @param uriConverter 
-	 * @param result 
-	 * @param baseUrl 
+	 * @param uriConverter
+	 * @param captureDate
+	 * @param baseUrl which must be absolute
 	 * @param tagName
 	 * @param attrName
 	 */
-	public static void markupTagREURIC (StringBuilder page, ResultURIConverter uriConverter, 
-			SearchResult result, String baseUrl, String tagName, String attrName) {
+	public static void markupTagREURIC(StringBuilder page,
+			ResultURIConverter uriConverter, String captureDate,
+			String baseUrl, String tagName, String attrName) {
 
 		Pattern tagPat = getPattern(tagName, attrName);
 		Matcher matcher = tagPat.matcher(page);
-		
+
 		int idx = 0;
-		while(matcher.find(idx)) {
+		while (matcher.find(idx)) {
 			String url = matcher.group(1);
 			int origUrlLength = url.length();
 			int attrStart = matcher.start(1);
 			int attrEnd = matcher.end(1);
 			String quote = "";
-			if(url.charAt(0) == '"') {
+			if (url.charAt(0) == '"') {
 				quote = "\"";
-				url = url.substring(1,url.length()-1);
-			} else if(url.charAt(0) == '\'') {
+				url = url.substring(1, url.length() - 1);
+			} else if (url.charAt(0) == '\'') {
 				quote = "'";
-				url = url.substring(1,url.length()-1);
-			} else if(url.charAt(0) == '\\') {
+				url = url.substring(1, url.length() - 1);
+			} else if (url.charAt(0) == '\\') {
 				quote = "\\\"";
-				url = url.substring(2,url.length()-2);
+				url = url.substring(2, url.length() - 2);
 			}
+			String finalUrl = UrlCanonicalizer.resolveUrl(baseUrl,url);
+			String replayUrl = quote
+					+ uriConverter.makeReplayURI(captureDate, finalUrl) + quote;
 
-			String replayUrl = quote + uriConverter.makeRedirectReplayURI(
-					result, url, baseUrl) + quote;
-			
 			int delta = replayUrl.length() - origUrlLength;
-			page.replace(attrStart,attrEnd,replayUrl);
+			page.replace(attrStart, attrEnd, replayUrl);
 			idx = attrEnd + delta;
 		}
+	}
+
+	private static String trimAttrValue(String value) {
+		if (value.charAt(0) == '"') {
+			value = value.substring(1, value.length() - 1);
+		} else if (value.charAt(0) == '\'') {
+			value = value.substring(1, value.length() - 1);
+		}
+		return value;
+	}
+
+	/**
+	 * find and return the ATTR value within a TAG tag inside the HTML document
+	 * within the StringBuffer page. returns null if no TAG-ATTR is found.
+	 * 
+	 * @param page
+	 * @param tag
+	 * @param attr
+	 * @return URL of base-href within page, or null if none is found.
+	 */
+	public static String getTagAttr(StringBuilder page, final String tag,
+			final String attr) {
+
+		String found = null;
+		Pattern daPattern = TagMagix.getPattern(tag, attr);
+		Matcher matcher = daPattern.matcher(page);
+		int idx = 0;
+
+		if (matcher.find(idx)) {
+			found = matcher.group(1);
+			found = trimAttrValue(found);
+		}
+
+		return found;
+	}
+
+	/**
+	 * Search through the HTML contained in page, returning the value of a
+	 * particular attribute. This version allows matching only tags that contain
+	 * a particular attribute-value pair, which is useful in extracting META tag
+	 * values, for example, in returning the value of the "content" attribute in
+	 * a META tag that also contains an attribute "http-equiv" with a value of
+	 * "Content-Type". All comparision is case-insensitive, but the value
+	 * returned is the original attribute value, as unmolested as possible.
+	 * 
+	 * If nothing matches, returns null.
+	 * 
+	 * 
+	 * @param page
+	 *            StringBuilding holding HTML
+	 * @param tag
+	 *            String containing tagname of interest
+	 * @param findAttr
+	 *            name of attribute within the tag to return
+	 * @param whereAttr
+	 *            only match tags with an attribute whereAttr
+	 * @param whereVal
+	 *            only match tags with whereAttr having this value
+	 * @return the value of attribute attr in tag where the tag also contains an
+	 *         attribute whereAttr, with value whereVal, or null if nothing
+	 *         matches.
+	 */
+	public static String getTagAttrWhere(StringBuilder page, final String tag,
+			final String findAttr, final String whereAttr, final String whereVal) {
+
+		Pattern tagPattern = TagMagix.getWholeTagPattern(tag);
+		Pattern findAttrPattern = getAttrPattern(findAttr);
+		Pattern whereAttrPattern = getAttrPattern(whereAttr);
+		Matcher tagMatcher = tagPattern.matcher(page);
+
+		while (tagMatcher.find()) {
+			String wholeTag = tagMatcher.group();
+			Matcher whereAttrMatcher = whereAttrPattern.matcher(wholeTag);
+			if (whereAttrMatcher.find()) {
+				String attrValue = whereAttrMatcher.group(1);
+				attrValue = trimAttrValue(attrValue);
+				if (attrValue.compareToIgnoreCase(whereVal) == 0) {
+					// this tag contains the right set, return the value for
+					// the attribute findAttr:
+					Matcher findAttrMatcher = findAttrPattern.matcher(wholeTag);
+					String value = null;
+					if (findAttrMatcher.find()) {
+						value = findAttrMatcher.group(1);
+						value = trimAttrValue(value);
+					}
+					return value;
+				}
+				// not the tag we want... maybe there is another: loop
+			}
+		}
+
+		return null;
 	}
 
 	/**
@@ -136,23 +281,6 @@ public class TagMagix {
 	 * @return URL of base-href within page, or null if none is found.
 	 */
 	public static String getBaseHref(StringBuilder page) {
-		String found = null;
-		Pattern baseHrefPattern = TagMagix.getPattern("BASE","HREF");
-		Matcher matcher = baseHrefPattern.matcher(page);
-		int idx = 0;
-
-		if(matcher.find(idx)) {
-			found = matcher.group(1);
-			if(found.charAt(0) == '"') {
-				found = found.substring(1,found.length()-1);
-			} else if(found.charAt(0) == '\'') {
-				found = found.substring(1,found.length()-1);
-			}
-		}
-
-		return found;
+		return getTagAttr(page, "BASE", "HREF");
 	}
-	
-
-	
 }
