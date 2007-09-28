56f43607b08d691b8c1859d976ff93a4e047f5ed
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-09-28 18:44:25 +0000

    INITIAL REV: small container wrapping ResourceIndex and ResourceStore
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2015 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/webapp/WaybackCollection.java
@@ -0,0 +1,51 @@
+/* WaybackCollection
+ *
+ * $Id$
+ *
+ * Created on 11:28:52 AM Sep 28, 2007.
+ *
+ * Copyright (C) 2007 Internet Archive.
+ *
+ * This file is part of wayback.
+ *
+ * wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.webapp;
+
+import org.archive.wayback.ResourceIndex;
+import org.archive.wayback.ResourceStore;
+
+/**
+ * Abstraction point for sharing document collection and index across multiple AccessPoints.
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public class WaybackCollection {
+	private ResourceStore resourceStore = null;
+	private ResourceIndex resourceIndex = null;
+	public ResourceStore getResourceStore() {
+		return resourceStore;
+	}
+	public void setResourceStore(ResourceStore resourceStore) {
+		this.resourceStore = resourceStore;
+	}
+	public ResourceIndex getResourceIndex() {
+		return resourceIndex;
+	}
+	public void setResourceIndex(ResourceIndex resourceIndex) {
+		this.resourceIndex = resourceIndex;
+	}
+}
