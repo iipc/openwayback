2c59376e542f73a8a7336f96d0f2e3d67ada02c3
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-11-05 23:38:24 +0000

    ROLLBACK: reverting to previous version -- title is now as before, with another subclass, TwoMonthTimelineResultsPartitioner having specialized Timeline title.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2070 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/query/resultspartitioner/TwoMonthResultsPartitioner.java
+++ wayback-core/src/main/java/org/archive/wayback/query/resultspartitioner/TwoMonthResultsPartitioner.java
@@ -67,7 +67,7 @@ public class TwoMonthResultsPartitioner extends ResultsPartitioner {
 		Calendar endMinusSecond = getCalendar();
 		endMinusSecond.setTime(end.getTime());
 		endMinusSecond.add(Calendar.SECOND,-1);
-		return wbRequest.getFormatter().format("ResultPartitions.month", 
-				start.getTime());
+		return wbRequest.getFormatter().format("ResultPartitions.twoMonth", 
+				start.getTime(),endMinusSecond.getTime());
 	}
 }
