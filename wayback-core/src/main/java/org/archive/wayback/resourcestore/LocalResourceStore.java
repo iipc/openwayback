d6112028e4d83eb4c5c65e91be54a4071273a0ac
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-11-28 03:13:01 +0000

    INITIAL REV: new LocalResourceStore implementation that allows compressed or uncompressed ARCs and WARCs
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2090 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/resourcestore/LocalResourceStore.java
@@ -0,0 +1,142 @@
+package org.archive.wayback.resourcestore;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.archive.wayback.ResourceStore;
+import org.archive.wayback.WaybackConstants;
+import org.archive.wayback.core.Resource;
+import org.archive.wayback.core.SearchResult;
+import org.archive.wayback.exception.ConfigurationException;
+import org.archive.wayback.exception.ResourceNotAvailableException;
+
+/**
+ * Class which implements a local ARC, WARC, ARC.gz, WARC.gz, ResourceStore
+ * including an optional automatic indexing thread 
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public class LocalResourceStore implements ResourceStore {
+
+	private File dataDir = null;
+	private AutoIndexThread indexThread = null;
+	
+	private ArcIndexer arcIndexer = new ArcIndexer();
+	private WarcIndexer warcIndexer = new WarcIndexer();
+	public final static String ARC_EXTENSION = ".arc";
+	public final static String ARC_GZ_EXTENSION = ".arc.gz";
+	public final static String WARC_EXTENSION = ".warc";
+	public final static String WARC_GZ_EXTENSION = ".warc.gz";
+	public final static String OPEN_EXTENSION = ".open";
+	private final static String[] SUFFIXES = {
+		"", ARC_EXTENSION, ARC_GZ_EXTENSION, WARC_EXTENSION, WARC_GZ_EXTENSION
+	};
+	private FilenameFilter filter = new ArcWarcFilenameFilter();
+	
+	public void init() throws ConfigurationException {
+		if(indexThread != null) {
+			indexThread.setStore(this);
+			indexThread.start();
+		}
+	}
+	protected String resultToFileName(SearchResult result) {
+		return result.get(WaybackConstants.RESULT_ARC_FILE);
+	}
+
+	protected long resultToOffset(SearchResult result) {
+		return Long.parseLong(result.get(WaybackConstants.RESULT_OFFSET));
+	}
+	
+	public File getLocalFile(String fileName) {
+		// try adding suffixes: empty string is first in the list
+		File file = null;
+		for(String suffix : SUFFIXES) {
+			file = new File(dataDir,fileName + suffix);
+			if(file.exists() && file.canRead()) {
+				return file;
+			}
+		}
+		// this might work if the full path is in the index...
+		file = new File(fileName);
+		if(file.exists() && file.canRead()) {
+			return file;
+		}
+		// doh.
+		return null;
+	}
+	
+	public Resource retrieveResource(SearchResult result) throws IOException,
+			ResourceNotAvailableException {
+		String fileName = resultToFileName(result);
+		long offset = resultToOffset(result);
+		File file = getLocalFile(fileName);
+		if (file == null) {
+			
+			// TODO: this needs to be prettied up for end user consumption..
+			throw new ResourceNotAvailableException("Cannot find ARC file ("
+					+ fileName + ")");
+		} else {
+
+			Resource r = ResourceFactory.getResource(file, offset);
+			return r;
+		}
+	}
+	
+	public Iterator<SearchResult> indexFile(File dataFile) throws IOException {
+		Iterator<SearchResult> itr = null;
+		
+		String name = dataFile.getName();
+		if(name.endsWith(ARC_EXTENSION)) {
+			itr = arcIndexer.iterator(dataFile);
+		} else if(name.endsWith(ARC_GZ_EXTENSION)) {
+			itr = arcIndexer.iterator(dataFile);			
+		} else if(name.endsWith(WARC_EXTENSION)) {
+			itr = warcIndexer.iterator(dataFile);
+		} else if(name.endsWith(WARC_GZ_EXTENSION)) {
+			itr = warcIndexer.iterator(dataFile);
+		}		
+		return itr;
+	}
+
+	public Iterator<String> fileNamesIterator() throws IOException {
+		if(dataDir != null) {
+			String[] files = dataDir.list(filter);
+			List<String> l = Arrays.asList(files);
+			return l.iterator();
+		}
+		return null;
+	}
+	
+	public File getDataDir() {
+		return dataDir;
+	}
+
+	public void setDataDir(File dataDir) {
+		this.dataDir = dataDir;
+	}
+	
+	private class ArcWarcFilenameFilter implements FilenameFilter {
+		public boolean accept(File dir, String name) {
+			File tmp = new File(dir,name);
+			if(tmp.isFile() && tmp.canRead()) {
+				return name.endsWith(ARC_EXTENSION) ||
+					name.endsWith(ARC_GZ_EXTENSION) ||
+					name.endsWith(WARC_GZ_EXTENSION) ||
+					name.endsWith(WARC_EXTENSION);
+			}
+			return false;
+		}
+	}
+
+	public AutoIndexThread getIndexThread() {
+		return indexThread;
+	}
+	public void setIndexThread(AutoIndexThread indexThread) {
+		this.indexThread = indexThread;
+	}
+}
