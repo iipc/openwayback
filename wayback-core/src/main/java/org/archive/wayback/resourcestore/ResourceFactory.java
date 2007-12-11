8f73cb61c8461c87ae36bebe3d912cc32546f974
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2007-12-11 22:28:41 +0000

    FEATURE: Now can create ARC/WARC Resources from URL + offset REFACTOR: moved common code into private methods
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2106 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/resourcestore/ResourceFactory.java
+++ wayback-core/src/main/java/org/archive/wayback/resourcestore/ResourceFactory.java
@@ -2,6 +2,7 @@ package org.archive.wayback.resourcestore;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.archive.io.ArchiveRecord;
 import org.archive.io.arc.ARCReader;
@@ -13,42 +14,92 @@ import org.archive.io.warc.WARCRecord;
 import org.archive.wayback.core.Resource;
 import org.archive.wayback.exception.ResourceNotAvailableException;
 
+/**
+ * Static factory class for constructing ARC/WARC Resources from 
+ * File/URL + offset.
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
 public class ResourceFactory {
-	
-	public static Resource getResource(File file, long offset) 
-	throws IOException, ResourceNotAvailableException {
+
+	public static Resource getResource(File file, long offset)
+			throws IOException, ResourceNotAvailableException {
 
 		Resource r = null;
 		String name = file.getName();
-		if(name.endsWith(LocalResourceStore.OPEN_EXTENSION)) {
-			name = name.substring(0, name.length() - 
-					LocalResourceStore.OPEN_EXTENSION.length());
+		if (name.endsWith(LocalResourceStore.OPEN_EXTENSION)) {
+			name = name.substring(0, name.length()
+					- LocalResourceStore.OPEN_EXTENSION.length());
 		}
-		if(name.endsWith(LocalResourceStore.ARC_EXTENSION) || 
-				name.endsWith(LocalResourceStore.ARC_GZ_EXTENSION)) {
+		if (isArc(name)) {
 
 			ARCReader reader = ARCReaderFactory.get(file);
-			ArchiveRecord rec = reader.get(offset);
-			if(!(rec instanceof ARCRecord)) {
-				throw new ResourceNotAvailableException("Bad ARCRecord format");
-			}
-			ArcResource ar = new ArcResource((ARCRecord) rec, reader);
-			ar.parseHeaders();
-			r = ar;
+			r = ARCArchiveRecordToResource(reader.get(offset),reader);
 
-		} else if(name.endsWith(LocalResourceStore.WARC_EXTENSION) || 
-				name.endsWith(LocalResourceStore.WARC_GZ_EXTENSION)) {
+		} else if (isWarc(name)) {
 
 			WARCReader reader = WARCReaderFactory.get(file);
-			ArchiveRecord rec = reader.get(offset);
-			if(!(rec instanceof WARCRecord)) {
-				throw new ResourceNotAvailableException("Bad WARCRecord format");
-			}
-			WarcResource wr = new WarcResource((WARCRecord) rec, reader);
-			wr.parseHeaders();
-			r = wr;
-		}		
-		
+			r = WARCArchiveRecordToResource(reader.get(offset),reader);
+
+		} else {
+			throw new ResourceNotAvailableException("Unknown extension");
+		}
+
 		return r;
+	}
+
+	public static Resource getResource(URL url, long offset)
+			throws IOException, ResourceNotAvailableException {
+		Resource r = null;
+		String name = url.getFile();
+		if (isArc(name)) {
+
+			ARCReader reader = ARCReaderFactory.get(url, offset);
+			r = ARCArchiveRecordToResource(reader.get(),reader);
+
+		} else if (isWarc(name)) {
+
+			WARCReader reader = WARCReaderFactory.get(url, offset);
+			r = WARCArchiveRecordToResource(reader.get(),reader);
+
+		} else {
+			throw new ResourceNotAvailableException("Unknown extension");
+		}
+		return r;
+	}
+
+	private static boolean isArc(final String name) {
+
+		return (name.endsWith(LocalResourceStore.ARC_EXTENSION)
+				|| name.endsWith(LocalResourceStore.ARC_GZ_EXTENSION));
+	}
+
+	private static boolean isWarc(final String name) {
+
+		return (name.endsWith(LocalResourceStore.WARC_EXTENSION)
+			|| name.endsWith(LocalResourceStore.WARC_GZ_EXTENSION));	
+	}
+	
+	private static Resource ARCArchiveRecordToResource(ArchiveRecord rec,
+			ARCReader reader) throws ResourceNotAvailableException, IOException {
+
+		if (!(rec instanceof ARCRecord)) {
+			throw new ResourceNotAvailableException("Bad ARCRecord format");
+		}
+		ArcResource ar = new ArcResource((ARCRecord) rec, reader);
+		ar.parseHeaders();
+		return ar;
+	}
+
+	private static Resource WARCArchiveRecordToResource(ArchiveRecord rec,
+			WARCReader reader) throws ResourceNotAvailableException, IOException {
+
+		if (!(rec instanceof WARCRecord)) {
+			throw new ResourceNotAvailableException("Bad WARCRecord format");
+		}
+		WarcResource wr = new WarcResource((WARCRecord) rec, reader);
+		wr.parseHeaders();
+		return wr;
 	}
 }
