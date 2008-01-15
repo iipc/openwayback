2bb2e9111d09bbf40ed17515ac33592fe509972d
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2008-01-15 02:17:09 +0000

    REFACTOR: moved UrlCanonicalizer to org.archive.wayback.util.url
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2130 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/util/url/UrlCanonicalizer.java
+++ wayback-core/src/main/java/org/archive/wayback/util/url/UrlCanonicalizer.java
@@ -22,7 +22,7 @@
  * along with Wayback; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.archive.wayback.util;
+package org.archive.wayback.util.url;
 
 import java.io.BufferedReader;
 import java.io.IOException;
