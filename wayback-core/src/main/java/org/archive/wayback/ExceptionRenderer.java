12197d1e0ade216a1f9daafe5fe56a1cbc1a3bf3
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2008-06-20 01:43:37 +0000

    REFACTOR: moving error rendering responsibility to separate interface, and out of ReplayRenderer.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2297 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/ExceptionRenderer.java
@@ -0,0 +1,59 @@
+/* ExceptionRenderer
+ *
+ * $Id$
+ *
+ * Created on 6:26:05 PM Jun 10, 2008.
+ *
+ * Copyright (C) 2008 Internet Archive.
+ *
+ * This file is part of wayback.
+ *
+ * wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.archive.wayback.core.WaybackRequest;
+import org.archive.wayback.exception.WaybackException;
+
+/**
+ * Implementors are responsible for drawing errors.
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+public interface ExceptionRenderer {
+	/**
+	 * Render the contents of a WaybackException in either html, javascript, or
+	 * css format, depending on the guessed context, so errors in embedded 
+	 * documents do not cause unneeded errors in the embedding document.
+	 * 
+	 * @param httpRequest
+	 * @param httpResponse
+	 * @param wbRequest
+	 * @param exception
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void renderException(HttpServletRequest httpRequest,
+			HttpServletResponse httpResponse, WaybackRequest wbRequest,
+			WaybackException exception)
+		throws ServletException, IOException;
+}
