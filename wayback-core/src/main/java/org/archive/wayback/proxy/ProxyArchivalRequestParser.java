62e13a9dc8d82c3a49a0e460c8da9d86b04597e3
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2008-10-09 23:39:15 +0000

    INITIAL REV: experimental archival URL query specification for proxy replay mode.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2599 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/proxy/ProxyArchivalRequestParser.java
@@ -0,0 +1,34 @@
+package org.archive.wayback.proxy;
+
+import java.util.List;
+
+import org.archive.wayback.RequestParser;
+import org.archive.wayback.archivalurl.requestparser.PathDatePrefixQueryRequestParser;
+import org.archive.wayback.archivalurl.requestparser.PathDateRangeQueryRequestParser;
+import org.archive.wayback.archivalurl.requestparser.PathPrefixDatePrefixQueryRequestParser;
+import org.archive.wayback.archivalurl.requestparser.PathPrefixDateRangeQueryRequestParser;
+import org.archive.wayback.requestparser.FormRequestParser;
+import org.archive.wayback.requestparser.OpenSearchRequestParser;
+
+public class ProxyArchivalRequestParser extends ProxyRequestParser {
+	private ProxyReplayRequestParser prrp = new ProxyReplayRequestParser();
+	protected RequestParser[] getRequestParsers() {
+		prrp.init();
+		RequestParser[] theParsers = {
+				prrp,
+				new PathDatePrefixQueryRequestParser(),
+				new PathDateRangeQueryRequestParser(),
+				new PathPrefixDatePrefixQueryRequestParser(),
+				new PathPrefixDateRangeQueryRequestParser(),
+				new OpenSearchRequestParser(),
+				new FormRequestParser() 
+				};
+		return theParsers;
+	}
+	public List<String> getLocalhostNames() {
+		return prrp.getLocalhostNames();
+	}
+	public void setLocalhostNames(List<String> localhostNames) {
+		prrp.setLocalhostNames(localhostNames);
+	}
+}
