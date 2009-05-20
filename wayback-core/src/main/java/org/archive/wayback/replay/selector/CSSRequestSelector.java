c49b4e938f58d3644364ce49753321f6da7c0b29
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2009-05-20 02:30:31 +0000

    INITIAL REV: new ReplayRendererSelector implementations which trigger when WaybackRequest context flags are set.
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2722 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/replay/selector/CSSRequestSelector.java
@@ -0,0 +1,48 @@
+/* CSSRequestSelector
+ *
+ * $Id$
+ *
+ * Created on 2:13:12 PM Feb 12, 2009.
+ *
+ * Copyright (C) 2009 Internet Archive.
+ *
+ * This file is part of wayback.
+ *
+ * wayback is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * any later version.
+ *
+ * wayback is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with wayback; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.archive.wayback.replay.selector;
+
+import org.archive.wayback.core.CaptureSearchResult;
+import org.archive.wayback.core.Resource;
+import org.archive.wayback.core.WaybackRequest;
+
+/**
+ *
+ *
+ * @author brad
+ * @version $Date$, $Revision$
+ */
+
+public class CSSRequestSelector extends BaseReplayRendererSelector {
+
+	/* (non-Javadoc)
+	 * @see org.archive.wayback.replay.selector.BaseReplayRendererSelector#canHandle(org.archive.wayback.core.WaybackRequest, org.archive.wayback.core.CaptureSearchResult, org.archive.wayback.core.Resource)
+	 */
+	@Override
+	public boolean canHandle(WaybackRequest wbRequest,
+			CaptureSearchResult result, Resource resource) {
+		return wbRequest.isCSSContext();
+	}
+}
