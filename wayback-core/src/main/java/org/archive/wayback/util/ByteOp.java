3ea15b5743f4cd08e3df3ad3358a775460e5ae61
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2010-01-12 22:24:04 +0000

    FEATURE: added copyStream() methods to drain bytes from an InputStream to an OutputStream
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@2944 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/util/ByteOp.java
+++ wayback-core/src/main/java/org/archive/wayback/util/ByteOp.java
@@ -24,7 +24,13 @@
  */
 package org.archive.wayback.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ByteOp {
+	public final static int BUFFER_SIZE = 4096;
+	
 	public static byte[] copy(byte[] src, int offset, int length) {
 		byte[] copy = new byte[length];
 		System.arraycopy(src, offset, copy, 0, length);
@@ -40,5 +46,29 @@ public class ByteOp {
 			}
 		}
 		return true;
+	}
+	/**
+	 * Write all bytes from is to os. Does not close either stream.
+	 * @param is to copy bytes from
+	 * @param os to copy bytes to
+	 * @throws IOException for usual reasons
+	 */
+	public static void copyStream(InputStream is, OutputStream os) 
+	throws IOException {
+		copyStream(is,os,BUFFER_SIZE);
+	}
+	/**
+	 * Write all bytes from is to os. Does not close either stream.
+	 * @param is to copy bytes from
+	 * @param os to copy bytes to
+	 * @param size number of bytes to buffer between read and write operations
+	 * @throws IOException for usual reasons
+	 */
+	public static void copyStream(InputStream is, OutputStream os, int size) 
+	throws IOException {
+		byte[] buffer = new byte[size];
+		for (int r = -1; (r = is.read(buffer, 0, size)) != -1;) {
+			os.write(buffer, 0, r);
+		}
 	}
 }
