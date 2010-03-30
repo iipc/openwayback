6e791a6e82e66533d3105316f95e4d5eb9e96ebf
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2010-03-30 22:34:57 +0000

    BUGFIX: now using (hopefully) correct resolving code: UURIFactory.resolve(UURI,String) instead of UURI.resolve(String,true)...
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3008 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/util/htmllex/ParseContext.java
+++ wayback-core/src/main/java/org/archive/wayback/util/htmllex/ParseContext.java
@@ -100,7 +100,7 @@ public class ParseContext {
 			url = url.substring(0,hashIdx);
 		}
 		try {
-			return baseUrl.resolve(url,true).toString() + frag;
+			return UURIFactory.getInstance(baseUrl, url).toString() + frag;
 		} catch (URIException e) {
 			e.printStackTrace();
 		}
