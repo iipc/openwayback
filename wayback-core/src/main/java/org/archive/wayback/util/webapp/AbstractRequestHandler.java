8851b7e188f72fe91f239362e82ac837299db775
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2010-04-27 22:34:03 +0000

    REFACTOR: moved HTTP GET form parsing static helper methods to this class
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3078 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/util/webapp/AbstractRequestHandler.java
+++ wayback-core/src/main/java/org/archive/wayback/util/webapp/AbstractRequestHandler.java
@@ -3,8 +3,12 @@
  */
 package org.archive.wayback.util.webapp;
 
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+
+import org.archive.wayback.exception.BadQueryException;
 
 
 /**
@@ -43,5 +47,56 @@ public abstract class AbstractRequestHandler implements RequestHandler {
 
 	public String translateRequestPathQuery(HttpServletRequest httpRequest) {
 		return RequestMapper.getRequestContextPathQuery(httpRequest);
+	}
+
+	/**
+	 * Extract the first value in the array mapped to by field in queryMap
+	 * @param queryMap the Map in which to search
+	 * @param field the field value desired
+	 * @return the first value in the array mapped to field in queryMap, if 
+	 * present, null otherwise
+	 */
+	public static String getMapParam(Map<String,String[]> queryMap,
+			String field) {
+		String arr[] = queryMap.get(field);
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		return arr[0];
+	}
+
+	/**
+	 * Extract the first value in the array mapped to by field in queryMap
+	 * @param queryMap the Map in which to search
+	 * @param field the field value desired
+	 * @return the first value in the array mapped to field in queryMap, if 
+	 * present. A BadQueryException is thrown if there is no appropriate value
+	 * @throws BadQueryException if there is nothing mapped to field, or if the
+	 * Array mapped to field is empty
+	 */
+	public static String getRequiredMapParam(Map<String,String[]> queryMap,
+			String field)
+	throws BadQueryException {
+		String value = getMapParam(queryMap,field);
+		if(value == null) {
+			throw new BadQueryException("missing field " + field);
+		}
+		if(value.length() == 0) {
+			throw new BadQueryException("empty field " + field);			
+		}
+		return value;
+	}
+
+	/**
+	 * Extract the first value in the array mapped to by field in queryMap
+	 * @param map the Map in which to search
+	 * @param param the field value desired
+	 * @return the first value in the array mapped to field in queryMap, if 
+	 * present, or an empty string otherwise
+	 */
+	public static String getMapParamOrEmpty(Map<String,String[]> map, 
+			String param) {
+		String val = getMapParam(map,param);
+		return (val == null) ? "" : val;
 	}
 }
