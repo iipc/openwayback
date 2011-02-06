f3e25f25a51aec57299fdd915d1f28d45484827d
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2011-02-06 14:35:02 +0000

    initial rev
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3392 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/test/java/org/archive/wayback/liveweb/URLtoARCCacherTest.java
@@ -0,0 +1,45 @@
+package org.archive.wayback.liveweb;
+
+
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.archive.wayback.exception.LiveDocumentNotAvailableException;
+import org.archive.wayback.util.ByteOp;
+
+import junit.framework.TestCase;
+
+public class URLtoARCCacherTest extends TestCase {
+	public void testSocketTimeout() throws Exception {
+	    MultiThreadedHttpConnectionManager connectionManager = null;
+	    HostConfiguration hostConfiguration = null;
+	    HttpClient http = null;
+    	connectionManager = new MultiThreadedHttpConnectionManager();
+    	hostConfiguration = new HostConfiguration();
+    	http = new HttpClient(connectionManager);
+    	http.setHostConfiguration(hostConfiguration);
+		HttpMethod method = null;
+//		String urlString = "http://wayback.archive-it.org:6100/one";
+		String urlString = "http://hello.com/one";
+		int socketTimeoutMS = 10;
+		int connectTimeoutMS = 100;
+    	connectionManager.getParams().setSoTimeout(socketTimeoutMS);
+    	connectionManager.getParams().setConnectionTimeout(connectTimeoutMS);
+		try {
+			method = new GetMethod(urlString);
+		} catch(IllegalArgumentException e) {
+			throw new LiveDocumentNotAvailableException("Url:" + urlString +
+					"does not look like an URL?");
+		}
+	    try {
+	    	int status = http.executeMethod(method);
+	    	System.out.println("Got response code: " + status);
+	    	ByteOp.copyStream(method.getResponseBodyAsStream(), System.out);
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+
+	}
+}
