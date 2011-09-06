431e88c3f2a78d3602b4144f4533e90e443484c8
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2011-09-06 04:18:00 +0000

    INITIAL REV: BlockLoader which loads from Local RandomAccessFiles, and a generic BlockLoader which tries to do "the right thing" with each location, choosing the correct block loader based on the URL/String prefix
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3531 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/resourceindex/ziplines/GenericBlockLoader.java
@@ -0,0 +1,78 @@
+/*
+ *  This file is part of the Wayback archival access software
+ *   (http://archive-access.sourceforge.net/projects/wayback/).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.archive.wayback.resourceindex.ziplines;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * Generic BlockLoader, which may simplify configuration - inspecting each 
+ * location to attempt to choose the correct BlockLoader:
+ *   HDFS, HTTP, or LocalFile
+ * @author brad
+ *
+ */
+public class GenericBlockLoader implements BlockLoader {
+	Http11BlockLoader http = null;
+	HDFSBlockLoader hdfs = null;
+	LocalFileBlockLoader local = null;
+	private String defaultFSURI;
+	public GenericBlockLoader() {
+		http = new Http11BlockLoader();
+//		hdfs = new HDFSBlockLoader(null);
+//		try {
+//			hdfs.init();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (URISyntaxException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		local = new LocalFileBlockLoader();
+	}
+	public void init() {
+		if(defaultFSURI != null) {
+			hdfs = new HDFSBlockLoader(defaultFSURI);
+			try {
+				hdfs.init();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public byte[] getBlock(String url, long offset, int length)
+			throws IOException {
+		if(hdfs != null && url.startsWith("hdfs://")) {
+			return hdfs.getBlock(url, offset, length);
+		} else if(url.startsWith("/")) {
+			return local.getBlock(url, offset, length);
+		}
+		return http.getBlock(url, offset, length);
+	}
+	public void setDefaultFSURI(String uri) {
+		defaultFSURI = uri;
+//		hdfs.setDefaultFSURI(uri);
+	}
+}
---
+++ wayback-core/src/main/java/org/archive/wayback/resourceindex/ziplines/LocalFileBlockLoader.java
@@ -0,0 +1,47 @@
+/*
+ *  This file is part of the Wayback archival access software
+ *   (http://archive-access.sourceforge.net/projects/wayback/).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.archive.wayback.resourceindex.ziplines;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Simple block loader which uses RandomAccessFiles to grab ranges of local 
+ * files.
+ * @author brad
+ *
+ */
+public class LocalFileBlockLoader implements BlockLoader {
+
+	public byte[] getBlock(String url, long offset, int length)
+			throws IOException {
+		File file = new File(url);
+		RandomAccessFile raf = new RandomAccessFile(file, "r");
+		raf.seek(offset);
+		if(raf.getFilePointer() != offset) {
+			throw new IOException("Failed seek("+offset+") in ("+url+")");
+		}
+		byte b[] = new byte[length];
+		raf.readFully(b);
+		return b;
+	}
+
+}
