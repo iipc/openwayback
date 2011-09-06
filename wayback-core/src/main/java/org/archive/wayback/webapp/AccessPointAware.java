82e47c9684a4de795192079128b7b65c732b06b9
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2011-09-06 03:43:17 +0000

    INITIAL REV: new interface for AccessPoint collaboraters to be made aware of their AccessPoint
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3512 69e27eb3-6e27-0410-b9c6-fffd7e226fab

---
+++ wayback-core/src/main/java/org/archive/wayback/webapp/AccessPointAware.java
@@ -0,0 +1,34 @@
+/*
+ *  This file is part of the Wayback archival access software
+ *   (http://archive-access.sourceforge.net/projects/wayback/).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.archive.wayback.webapp;
+
+/**
+ * Interface for object which need to be made aware of the AccessPoint within
+ * which they are operating
+ * 
+ * @author brad
+ *
+ */
+public interface AccessPointAware {
+	/**
+	 * @param accessPoint in use with this object
+	 */
+	public void setAccessPoint(AccessPoint accessPoint);
+}
