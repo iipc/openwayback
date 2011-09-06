c7f5369d8724b8da40cf56246928dfdea70d737b
bradtofel <bradtofel@69e27eb3-6e27-0410-b9c6-fffd7e226fab>
2011-09-06 03:57:31 +0000

    FEATURE: a sad attempt to allow construction without a given HDFS URL
    git-svn-id: http://archive-access.svn.sourceforge.net/svnroot/archive-access/trunk/archive-access/projects@3519 69e27eb3-6e27-0410-b9c6-fffd7e226fab

--- wayback-core/src/main/java/org/archive/wayback/resourceindex/ziplines/HDFSBlockLoader.java
+++ wayback-core/src/main/java/org/archive/wayback/resourceindex/ziplines/HDFSBlockLoader.java
@@ -16,9 +16,17 @@ public class HDFSBlockLoader implements BlockLoader {
 		this.defaultFSURI = defaultFSURI;
 	}
 	public void init() throws IOException, URISyntaxException {
-		Configuration c = new Configuration();
-		c.set("fs.default.name",defaultFSURI);
-		fs = FileSystem.get(new URI(defaultFSURI),c);
+		if(defaultFSURI == null) {
+			Configuration c = new Configuration();
+//			String defaultURI = "hdfs://hadoop-name.example.org/";
+//			c.set("fs.default.name",defaultURI);
+//			fs = FileSystem.get(new URI(defaultURI),c);		
+			fs = FileSystem.get(c);
+		} else {
+			Configuration c = new Configuration();
+			c.set("fs.default.name",defaultFSURI);
+			fs = FileSystem.get(new URI(defaultFSURI),c);
+		}
 	}
 
 	public byte[] getBlock(String url, long offset, int length)
@@ -42,5 +50,16 @@ public class HDFSBlockLoader implements BlockLoader {
 	 */
 	public void setDefaultFSURI(String defaultFSURI) {
 		this.defaultFSURI = defaultFSURI;
+		Configuration c = new Configuration();
+		c.set("fs.default.name",defaultFSURI);
+		try {
+			fs = FileSystem.get(new URI(defaultFSURI),c);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
