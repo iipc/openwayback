006db20f72cd5069d05d3059dffc5ef9b1180169
Ilya Kreymer <ilya@archive.org>
2013-08-15 09:46:19 -0700

    Update Plugin work

--- contrib/ia/plugins/chrome/wayback.js
+++ contrib/ia/plugins/chrome/wayback.js
@@ -1,62 +1,114 @@
-var waybackHost = "wayback.archive-it.org";
-var waybackPrefix = "http://" + waybackHost + "/";
-var waybackProxyPort = 8081;
-
-var proxyColl = "3701";
-
-var proxyMode = false;
-
 var tabIdToUrl = {};
 
 var archivalURLRegex = /([^/]*)\/(\d{1,14})([a-z]{2}_)*\/(.*)/;
 
+var knownWaybackPrefixs = 
+  ["http://web.archive.org/", 
+   "http://wayback.archive-it.org/", 
+   "http://webharvest.gov/"];
 
-function extractTopLevelInfo(url)
+function findWaybackPrefix(url)
+{
+  for (i in knownWaybackPrefixs) {
+    if (url.indexOf(knownWaybackPrefixs[i]) == 0) {
+      return knownWaybackPrefixs[i];
+    }
+  }
+  
+  return null;
+}
+
+function extractWaybackReplayInfo(waybackPrefix, url, matchOnly)
 {
   var rest = url.substr(waybackPrefix.length);
   var parts = rest.match(archivalURLRegex);
   
   if (!parts) {
-    return;
+    return null;
   }
   
-  return {"fullWaybackUrl": url, "coll": parts[1], "timestamp": parts[2], "mod": parts[3], "url": parts[4]};
+  if (matchOnly) {
+    return true;
+  }
+  
+  return {"fullWaybackUrl": url,
+    
+          "waybackPrefix": waybackPrefix,
+          "coll": parts[1], 
+          "timestamp": parts[2], 
+          "mod": parts[3], 
+          "url": parts[4]};
 }
 
 
 function makeReplayUrl(replayInfo, url)
 {
-  return waybackPrefix + replayInfo.coll + "/" + replayInfo.timestamp + "id_/" + url;
+  // Partial Wayback URL
+  if (url.indexOf(replayInfo.waybackPrefix) == 0) {
+    
+    var urlFromPrefix = url.substr(replayInfo.waybackPrefix.length);
+    var path;
+    
+    if (urlFromPrefix && (urlFromPrefix.indexOf(replayInfo.coll) == 0)) {
+      // Don't rewrite urls that have collection path
+      return url;
+      //var urlFromColl = urlFromPrefix.substr(replayInfo.coll.length + 1);
+      //path = urlFromColl;
+    } else {
+      path = urlFromPrefix;
+    }
+    
+    if (path) {
+      var fullPath = replayInfo.fullWaybackUrl;
+      var lastSlash = fullPath.lastIndexOf("/");
+      if (lastSlash > 0) {
+        fullPath = replayInfo.fullWaybackUrl.substr(0, lastSlash + 1);
+      } else {
+        fullPath += "/";
+      }
+      fullPath += path;
+      console.log(url + " -> " + fullPath);
+      return fullPath;
+    }
+  }  
+  
+  var newUrl = replayInfo.waybackPrefix + replayInfo.coll + "/" + replayInfo.timestamp + "id_/" + url;  
+  return newUrl;
 }
 
 
 function isWaybackReplayTab(tabId)
 {
+  if (proxyMode) {
+    return true;
+  }
+  
   return (tabIdToUrl && tabIdToUrl[tabId]);
 }
 
 
 function archivalRedirect(details)
-{  
-  if (details.url.indexOf("#wb_pass") >= 0) {
-    return {};
-  }
+{
+        
+  // Top Level Request
+  if (details.type == "main_frame") {
+    var waybackPrefix = findWaybackPrefix(details.url);
     
-  if (details.url.indexOf(waybackPrefix) >= 0) {
-    if (details.type == "main_frame") {
-      replayInfo = extractTopLevelInfo(details.url);
+    if (waybackPrefix) {
+      replayInfo = extractWaybackReplayInfo(waybackPrefix, details.url);
       
       if (replayInfo) {
+        
+        tabIdToUrl[details.tabId] = replayInfo;
         
         // Redirect to id_       
         if (!replayInfo.mod || replayInfo.mod == "") {
            //console.log(replayInfo.mod);
            return { redirectUrl: makeReplayUrl(replayInfo, replayInfo.url) };
         }
-      
-        tabIdToUrl[details.tabId] = replayInfo;
       }
     }
+    
     return {};
   }
   
@@ -64,13 +116,23 @@ function archivalRedirect(details)
     return {};
   }
   
+  // If not in wayback tab, pass through
   if (!isWaybackReplayTab(details.tabId)) {
     return {};
   }
       
   var replayInfo = tabIdToUrl[details.tabId];
   
-  return { redirectUrl: makeReplayUrl(replayInfo, details.url) };
+  // If already a wayback url, pass through
+  if (extractWaybackReplayInfo(replayInfo.waybackPrefix, details.url, true)) {
+    return {};
+  }
+  
+  var url = makeReplayUrl(replayInfo, details.url);
+  
+  //console.log(details.url + " -> " + url);
+   
+  return { redirectUrl: url };
 }
 
 
@@ -82,18 +144,24 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeinfo, tab) {
   }
 });
 
+
 // Proxy Mode
+var proxyHost = "wayback.archive-it.org";
+var proxyPort = 8081;
+var proxyColl = "3701";
+var proxyMode = false;
+
 if (proxyMode) {
 
   var config = {
     mode: "fixed_servers",
     rules: {
-      proxyForHttp: {
-        host: waybackHost,
-        port: waybackProxyPort
+      singleProxy: {
+        host: proxyHost,
+        port: proxyPort
       },
       
-      bypassList: [waybackHost]
+      bypassList: [proxyHost]
     }
   };
   
@@ -101,18 +169,22 @@ if (proxyMode) {
       {value: config, scope: 'regular'},
       function() {});
     
-  /*    
+      
   chrome.webRequest.onBeforeRequest.addListener(
     function(details)
     {
+      // Rewrite https -> http as https proxying not supported
       if (details.url.indexOf("https://") == 0) {
-        return { redirectUrl: "http://" + details.url.substr("https://".length) };
+        httpUrl = "http://" + details.url.substr("https://".length);
+        console.log(details.url + " -> " + httpUrl);
+        
+        return { redirectUrl: httpUrl };
       } 
     },
     
     {urls: ["<all_urls>"]},
     ["blocking"]
-  );*/
+  );
   
   chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details)
@@ -123,7 +195,8 @@ if (proxyMode) {
         } else {
           headers = [];
         }
-        headers.push({"name": "X-Coll", "value": proxyColl});
+        headers.push({"name": "X-Wayback-Proxy-Coll", "value": proxyColl});
+        //headers.push({"name": "Proxy-Timestamp", "value": "20130605191057"});
         return { requestHeaders: headers };
       }
       
