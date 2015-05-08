b9cc524d4f30e9d89d86ecd50606e306775d325b
Kristinn Sigurðsson <kris.sigur@gmail.com>
2015-05-08 13:06:48 +0000

    Added class to preserve backwards compatability, for now.

---
+++ wayback-core/src/main/java/org/archive/wayback/liveweb/RemoteLiveWebCache.java
@@ -0,0 +1,11 @@
+package org.archive.wayback.liveweb;
+
+/**
+ * This class is provided to maintain backwards compatibility.
+ * 
+ * @deprecated As of release 2.2.0, replaced by {@link ArcRemoteLiveWebCache}. This class will be removed in 3.0.0
+ */
+@Deprecated
+public class RemoteLiveWebCache extends ArcRemoteLiveWebCache {
+
+}
