VirginiaMaxwell/wayback

TimeMaps without Mementos
Respond with 404 on TimeMap requests for URIs without Mementos.

https://tools.ietf.org/html/rfc7089#section-4.4

wayback-core/src/main/java/org/archive/wayback/memento/MementoUtils.java
@@ -54,7 +54,12 @@ public class MementoUtils implements MementoConstants {
 			WaybackRequest wbRequest, HttpServletResponse response)
 			throws IOException {
 		response.setContentType("application/link-format");
-		printLinkTimemap(results, wbRequest, response.getWriter());
+		if(results.size() == 0){
+			response.setStatus(response.SC_NOT_FOUND);
+			response.getWriter().println(makeLink(getTimegateUrl(wbRequest.getAccessPoint(), wbRequest.getRequestUrl()), TIMEGATE));
+		} else {
+			printLinkTimemap(results, wbRequest, response.getWriter());
+		}
 	}
 
 	public static void printLinkTimemap(CaptureSearchResults results,

iipc/openwayback

wayback-core/src/main/java/org/archive/wayback/memento/MementoUtils.java
@@ -53,7 +53,12 @@ public class MementoUtils implements MementoConstants {
 			WaybackRequest wbRequest, HttpServletResponse response)
 			throws IOException {
 		response.setContentType("application/link-format");
-		printLinkTimemap(results, wbRequest, response.getWriter());
+		if(results.size() == 0){
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			response.getWriter().println(makeLink(getTimegateUrl(wbRequest.getAccessPoint(), wbRequest.getRequestUrl()), TIMEGATE));
+		} else {
+			printLinkTimemap(results, wbRequest, response.getWriter());
+		}
 	}
 
 	public static void printLinkTimemap(CaptureSearchResults results,