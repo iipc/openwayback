4cae1d4dffe2070d0b755e61a3d700982a619a3d
Barbara Miller <galgeek@me.com>
2019-01-24 17:16:20 -0800

    icu4j charset detector

--- wayback-core/src/main/java/org/archive/wayback/replay/charset/UniversalChardetSniffer.java
+++ wayback-core/src/main/java/org/archive/wayback/replay/charset/UniversalChardetSniffer.java
@@ -3,42 +3,39 @@ package org.archive.wayback.replay.charset;
 import java.io.IOException;
 
 import org.archive.wayback.core.Resource;
-import org.mozilla.universalchardet.UniversalDetector;
+import com.ibm.icu.text.CharsetDetector;
+import com.ibm.icu.text.CharsetMatch;
 
 /**
- * {@link EncodingSniffer} that runs {@link UniversalDetector} on
+ * {@link EncodingSniffer} that runs {@link icu4j CharacterDetector} on
  * the content.
- * <p>Note: as of version 1.0.3, UniversalDetector returns {@code null}
- * for ASCII-only text encoded as either {@code UTF-8} or {@code UTF-16}.</p>
  */
 public class UniversalChardetSniffer extends BaseEncodingSniffer {
 	// hand off this many bytes to the chardet library
-	protected final static int MAX_CHARSET_READAHEAD = 73728;
+	protected final static int MAX_CHARSET_READAHEAD = 98304;
 
 	@Override
 	public String sniff(Resource resource) {
 		String charsetName = null;
 
 		byte[] bbuffer = new byte[MAX_CHARSET_READAHEAD];
-		// (1)
-		UniversalDetector detector = new UniversalDetector(null);
 
-		// (2)
+		CharsetDetector detector = new CharsetDetector();
+
 		resource.mark(MAX_CHARSET_READAHEAD);
 		try {
-			int len = resource.read(bbuffer, 0, MAX_CHARSET_READAHEAD);
+			resource.read(bbuffer, 0, MAX_CHARSET_READAHEAD);
 			resource.reset();
-			detector.handleData(bbuffer, 0, len);
+
+			detector.setText(bbuffer);
+			CharsetMatch[] matches = detector.detectAll();
+			if (matches != null && matches.length > 0) {
+				charsetName = matches[0].getName();
+			}
+
 		} catch (IOException ex) {
 			//
 		}
-		// (3)
-		detector.dataEnd();
-		// (4)
-		charsetName = detector.getDetectedCharset();
-
-		// (5)
-		detector.reset();
 		if (isCharsetSupported(charsetName)) {
 			return charsetName;
 		}
