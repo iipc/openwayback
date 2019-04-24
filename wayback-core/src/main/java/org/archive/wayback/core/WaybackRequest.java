5d8a6f22305bb49512152891209cf969b51837b3
peveikko <petteri.veikkolainen@helsinki.fi>
2019-04-24 13:46:28 +0300

    Update WaybackRequest.java

--- wayback-core/src/main/java/org/archive/wayback/core/WaybackRequest.java
+++ wayback-core/src/main/java/org/archive/wayback/core/WaybackRequest.java
@@ -763,7 +763,7 @@ public class WaybackRequest {
 		// is known, adding an implied "http://" scheme if there doesn't appear
 		// to be a scheme..
 		// TODO: make the default "http://" configurable.
-		if (!urlStr.startsWith(UrlOperations.HTTP_SCHEME)) {
+		if (!urlStr.startsWith(UrlOperations.HTTP_SCHEME) && !urlStr.startsWith(UrlOperations.HTTPS_SCHEME)) {
 	    		if(urlStr.startsWith("http:/")) {
 	    			urlStr = UrlOperations.HTTP_SCHEME + urlStr.substring(6);
 	    		} else if(urlStr.startsWith("https:/")) {
